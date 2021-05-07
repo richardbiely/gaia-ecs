ddaa6e7618cf69db6cbac2819eb8f3e83b92302a
Richard Biely <richardbiely@gmail.com>
2021-05-07 20:20:25 +0200

    Compile time checks for multiple components of the same type in querries + minor fixes

---
+++ include/gaia/utils/utility.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <type_traits>
+
+namespace gaia {
+namespace utils {
+template <typename...> inline constexpr auto is_unique = std::true_type{};
+
+template <typename T, typename... Rest>
+inline constexpr auto is_unique<T, Rest...> =
+    std::bool_constant<(!std::is_same_v<T, Rest> && ...) &&
+                       is_unique<Rest...>>{};
+} // namespace utils
+} // namespace gaia
