618d58380bce8870cdcc6c372a0405ea8051ea02
Richard Biely <richardbiely@gmail.com>
2021-05-12 20:22:42 +0200

    Reflection part of the code separated + renames of global methods

---
+++ include/gaia/utils/reflection.h
@@ -0,0 +1,94 @@
+#pragma once
+
+namespace gaia {
+	namespace utils {
+
+#pragma region "Tuple to struct conversion"
+
+		template <class S, unsigned... Is, class Tupple>
+		S tuple_to_struct(std::index_sequence<Is...>, Tupple&& tup) {
+			using std::get;
+			return {get<Is>(std::forward<Tupple>(tup))...};
+		}
+
+		template <class S, class Tupple> S tuple_to_struct(Tupple&& tup) {
+			using T = std::remove_reference_t<Tupple>;
+
+			return tuple_to_struct<S>(
+					std::make_index_sequence<std::tuple_size<T>{}>{},
+					std::forward<Tupple>(tup));
+		}
+
+#pragma endregion
+
+#pragma region "Struct to tuple conversion"
+
+		// Check is a type T is constructible such as T{Args...}
+		struct any_type {
+			template <class T> constexpr operator T(); // non explicit
+		};
+
+		template <class T, class... TArgs>
+		decltype(void(T{std::declval<TArgs>()...}), std::true_type{})
+		is_braces_constructible(int);
+
+		template <class, class...> std::false_type is_braces_constructible(...);
+		template <class T, class... TArgs>
+		using is_braces_constructible_t =
+				decltype(is_braces_constructible<T, TArgs...>(0));
+
+		// Converts a struct to a tuple (struct necessary to support
+		// initialization via Struct{x,y,...,z})
+		template <class T> auto struct_to_tuple(T&& object) noexcept {
+			using type = std::decay_t<T>;
+			// Don't support empty structs. They have no data.
+			// We also want compilation to fail for structs with many members so we
+			// can handle them here That shouldn't be necessary, though for we plan
+			// to support only structs with little amount of arguments.
+				if constexpr (is_braces_constructible_t<
+													type, any_type, any_type, any_type, any_type,
+													any_type, any_type, any_type, any_type>{}) {
+					auto&& [p1, p2, p3, p4, p5, p6, p7, p8] = object;
+					return std::make_tuple(p1, p2, p3, p4, p5, p6, p7, p8);
+				} else if constexpr (is_braces_constructible_t<
+																 type, any_type, any_type, any_type, any_type,
+																 any_type, any_type, any_type>{}) {
+					auto&& [p1, p2, p3, p4, p5, p6, p7] = object;
+					return std::make_tuple(p1, p2, p3, p4, p5, p6, p7);
+				} else if constexpr (is_braces_constructible_t<
+																 type, any_type, any_type, any_type, any_type,
+																 any_type, any_type>{}) {
+					auto&& [p1, p2, p3, p4, p5, p6] = object;
+					return std::make_tuple(p1, p2, p3, p4, p5, p6);
+				} else if constexpr (is_braces_constructible_t<
+																 type, any_type, any_type, any_type, any_type,
+																 any_type>{}) {
+					auto&& [p1, p2, p3, p4, p5] = object;
+					return std::make_tuple(p1, p2, p3, p4, p5);
+				} else if constexpr (is_braces_constructible_t<
+																 type, any_type, any_type, any_type,
+																 any_type>{}) {
+					auto&& [p1, p2, p3, p4] = object;
+					return std::make_tuple(p1, p2, p3, p4);
+				} else if constexpr (is_braces_constructible_t<
+																 type, any_type, any_type, any_type>{}) {
+					auto&& [p1, p2, p3] = object;
+					return std::make_tuple(p1, p2, p3);
+				} else if constexpr (is_braces_constructible_t<
+																 type, any_type, any_type>{}) {
+					auto&& [p1, p2] = object;
+					return std::make_tuple(p1, p2);
+				} else if constexpr (is_braces_constructible_t<type, any_type>{}) {
+					auto&& [p1] = object;
+					return std::make_tuple(p1);
+				}
+				// Let's not support defaults. We don't want to allow too many types
+				// because they indicate some wrong usage of ECS.
+				else {
+					return std::make_tuple();
+				}
+		}
+
+#pragma endregion
+	} // namespace utils
+} // namespace gaia
