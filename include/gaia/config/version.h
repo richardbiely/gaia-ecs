32dd9102c597fefe38df6250508c6e32077eceba
Richard Biely <richardbiely@gmail.com>
2021-12-18 15:37:08 +0100

    Version bump

--- include/gaia/config/version.h
+++ include/gaia/config/version.h
@@ -1,5 +1,8 @@
 #pragma once
 
+// Breaking changes and big features
 #define GAIA_VERSION_MAJOR 0
-#define GAIA_VERSION_MINOR 1
+// Smaller changes and features
+#define GAIA_VERSION_MINOR 2
+// Fixes and tweaks
 #define GAIA_VERSION_PATCH 0
