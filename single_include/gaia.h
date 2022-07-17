65f36b546291f42392ca33a284ee0d1cda62f3a8
Richard Biely <richardbiely@gmail.com>
2022-07-17 22:26:42 +0200

    Update gaia.h

--- single_include/gaia.h
+++ single_include/gaia.h
@@ -180,6 +180,10 @@
 	#define GAIA_GCC_WARNING_DISABLE(warningId)
 #endif
 
+#if (!defined(__GNUC__) && !defined(__clang__)) || defined(__pnacl__) || defined(__EMSCRIPTEN__)
+	#define GAIA_HAS_NO_INLINE_ASSEMBLY 1
+#endif
+
 // Breaking changes and big features
 #define GAIA_VERSION_MAJOR 0
 // Smaller changes and features
@@ -199,7 +203,7 @@
 // General settings
 //------------------------------------------------------------------------------
 
-//! If enabled, GAIA_DEBUG is defined despite using a "Release" bulid configuration for example
+//! If enabled, GAIA_DEBUG is defined despite using a "Release" build configuration for example
 #define GAIA_FORCE_DEBUG 0
 //! If enabled, no asserts are thrown even in debug builds
 #define GAIA_DISABLE_ASSERTS 0
@@ -260,10 +264,12 @@
 //! slows things down but enables better security and diagnostics.
 //! Suitable for debug builds first and foremost. Therefore, it is
 //! enabled by default for debud builds.
-#if !defined(NDEBUG) || defined(_DEBUG) || GAIA_FORCE_DEBUG
-	#define GAIA_DEBUG 1
-#else
-	#define GAIA_DEBUG 0
+#if !defined(GAIA_DEBUG)
+	#if !defined(NDEBUG) || defined(_DEBUG) || GAIA_FORCE_DEBUG
+		#define GAIA_DEBUG 1
+	#else
+		#define GAIA_DEBUG 0
+	#endif
 #endif
 
 #if defined(GAIA_DISABLE_ASSERTS)
@@ -271,7 +277,16 @@
 	#define GAIA_ASSERT(condition) (void(0))
 #elif !defined(GAIA_ASSERT)
 	#include <cassert>
-	#define GAIA_ASSERT(condition) assert(condition)
+	#if GAIA_DEBUG
+		#define GAIA_ASSERT(condition)                                                                                     \
+			{                                                                                                                \
+				bool cond_ret = condition;                                                                                     \
+				assert(cond_ret);                                                                                              \
+				DoNotOptimize(cond_ret)                                                                                        \
+			}
+	#else
+		#define GAIA_ASSERT(condition) assert(condition);
+	#endif
 #endif
 
 #if defined(GAIA_ECS_DIAGS)
@@ -2612,6 +2627,45 @@ namespace gaia {
 #endif
 			}
 		}
+
+		// The DoNotOptimize(...) function can be used to prevent a value or
+// expression from being optimized away by the compiler. This function is
+// intended to add little to no overhead.
+// See: https://youtu.be/nXaxk27zwlk?t=2441
+#if GAIA_HAS_NO_INLINE_ASSEMBLY
+		template <class T>
+		inline GAIA_FORCEINLINE void DoNotOptimize(T const& value) {
+			asm volatile("" : : "r,m"(value) : "memory");
+		}
+
+		template <class T>
+		inline GAIA_FORCEINLINE void DoNotOptimize(T& value) {
+	#if defined(__clang__)
+			asm volatile("" : "+r,m"(value) : : "memory");
+	#else
+			asm volatile("" : "+m,r"(value) : : "memory");
+	#endif
+		}
+#else
+		namespace internal {
+			inline GAIA_FORCEINLINE void UseCharPointer(char const volatile* var) {
+				(void)var;
+			}
+		} // namespace internal
+
+	#if defined(_MSC_VER)
+		template <class T>
+		inline GAIA_FORCEINLINE void DoNotOptimize(T const& value) {
+			internal::UseCharPointer(&reinterpret_cast<char const volatile&>(value));
+			_ReadWriteBarrier();
+		}
+	#else
+		template <class T>
+		inline GAIA_FORCEINLINE void DoNotOptimize(T const& value) {
+			internal::UseCharPointer(&reinterpret_cast<char const volatile&>(value));
+		}
+	#endif
+#endif
 
 	} // namespace utils
 } // namespace gaia
