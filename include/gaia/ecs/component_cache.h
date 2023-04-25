richardbiely/gaia-ecs

commit 1df450c6cc8d676693cc7647bf229e9bc1542035

Tweaked: emplace replaced with try_emplace where possible

include/gaia/ecs/component_cache.h
@@ -36,13 +36,10 @@ namespace gaia {
 				const auto index = utils::type_info::index<U>();
 
 				if (m_infoCreateByIndex.find(index) == m_infoCreateByIndex.end())
-					m_infoCreateByIndex.emplace(index, ComponentInfoCreate::Create<U>());
+					(void)m_infoCreateByIndex.try_emplace(index, ComponentInfoCreate::Create<U>());
 
 				{
-					const auto res = m_infoByIndex.emplace(index, nullptr);
-					if (res.second)
-						res.first->second = ComponentInfo::Create<U>();
-
+					const auto res = m_infoByIndex.try_emplace(index, ComponentInfo::Create<U>());
 					return res.first->second;
 				}
 			}

include/gaia/ecs/archetype.h
@@ -327,14 +327,14 @@ namespace gaia {
 #if GAIA_ARCHETYPE_GRAPH
 			//! Create an edge in the graph leading from this archetype to \param archetypeId via component \param info.
 			void AddEdgeArchetypeRight(ComponentType type, const ComponentInfo* info, uint32_t archetypeId) {
-				GAIA_ASSERT(left->FindAddEdgeArchetypeId(type, info) == (uint32_t)-1);
-				(void)edgesAdd[type].emplace(info->infoIndex, ArchetypeGraphEdge{archetypeId});
+				[[maybe_unused]] const auto ret = edgesAdd[type].try_emplace(info->infoIndex, ArchetypeGraphEdge{archetypeId});
+				GAIA_ASSERT(ret.second);
 			}
 
 			//! Create an edge in the graph leading from this archetype to \param archetypeId via component \param info.
 			void AddEdgeArchetypeLeft(ComponentType type, const ComponentInfo* info, uint32_t archetypeId) {
-				GAIA_ASSERT(right->FindDelEdgeArchetypeId(type, info) == (uint32_t)-1);
-				(void)edgesDel[type].emplace(info->infoIndex, ArchetypeGraphEdge{archetypeId});
+				[[maybe_unused]] const auto ret = edgesDel[type].try_emplace(info->infoIndex, ArchetypeGraphEdge{archetypeId});
+				GAIA_ASSERT(ret.second);
 			}
 
 			uint32_t FindAddEdgeArchetypeId(ComponentType type, const ComponentInfo* info) const {

include/gaia/ecs/command_buffer.h
@@ -347,7 +347,7 @@ namespace gaia {
 			static constexpr CmdBufferCmdFunc CommandBufferCmd[] = {
 					// CREATE_ENTITY
 					[](CommandBufferCtx& ctx) {
-						[[maybe_unused]] const auto res = ctx.entityMap.emplace(ctx.entities++, ctx.world.CreateEntity());
+						[[maybe_unused]] const auto res = ctx.entityMap.try_emplace(ctx.entities++, ctx.world.CreateEntity());
 						GAIA_ASSERT(res.second);
 					},
 					// CREATE_ENTITY_FROM_ARCHETYPE
@@ -356,14 +356,14 @@ namespace gaia {
 						auto* pArchetype = (Archetype*)ptr;
 						ctx.dataOffset += sizeof(void*);
 						[[maybe_unused]] const auto res =
-								ctx.entityMap.emplace(ctx.entities++, ctx.world.CreateEntity(*pArchetype));
+								ctx.entityMap.try_emplace(ctx.entities++, ctx.world.CreateEntity(*pArchetype));
 						GAIA_ASSERT(res.second);
 					},
 					// CREATE_ENTITY_FROM_ENTITY
 					[](CommandBufferCtx& ctx) {
 						Entity entityFrom = utils::unaligned_ref<Entity>((void*)&ctx.data[ctx.dataOffset]);
 						ctx.dataOffset += sizeof(Entity);
-						[[maybe_unused]] const auto res = ctx.entityMap.emplace(ctx.entities++, ctx.world.CreateEntity(entityFrom));
+						[[maybe_unused]] const auto res = ctx.entityMap.try_emplace(ctx.entities++, ctx.world.CreateEntity(entityFrom));
 						GAIA_ASSERT(res.second);
 					},
 					// DELETE_ENTITY

include/gaia/ecs/system_base.h
@@ -206,7 +206,7 @@ namespace gaia {
 			T* CreateSystem(const char* name) {
 				GAIA_SAFE_CONSTEXPR auto hash = utils::type_info::hash<std::decay_t<T>>();
 
-				const auto res = m_systemsMap.emplace(utils::direct_hash_key{hash}, nullptr);
+				const auto res = m_systemsMap.try_emplace(utils::direct_hash_key{hash}, nullptr);
 				if GAIA_UNLIKELY (!res.second)
 					return (T*)res.first->second;
 

commit 99f2a88a0c54bfd690877731fa51771a97b8384f

Fixed: Accidental performance regression from 1df450c (and made it even better than the original)

include/gaia/ecs/component_cache.h
@@ -37,9 +37,15 @@ namespace gaia {
 				using U = typename DeduceComponent<T>::Type;
 
 				const auto index = utils::type_info::index<U>();
-				(void)m_infoCreateByIndex.try_emplace(index, ComponentInfoCreate::Create<U>());
-				const auto res = m_infoByIndex.try_emplace(index, ComponentInfo::Create<U>());
-				return res.first->second;
+
+				const auto res1 = m_infoCreateByIndex.try_emplace(index, ComponentInfoCreate{});
+				if GAIA_UNLIKELY (res1.second)
+					res1.first->second = ComponentInfoCreate::Create<U>();
+
+				const auto res2 = m_infoByIndex.try_emplace(index, nullptr);
+				if GAIA_UNLIKELY (res2.second)
+					res2.first->second = ComponentInfo::Create<U>();
+				return res2.first->second;
 			}
 
 			template <typename T>
@@ -54,8 +60,8 @@ namespace gaia {
 			template <typename T>
 			GAIA_NODISCARD const ComponentInfo* GetComponentInfo() const {
 				using U = typename DeduceComponent<T>::Type;
-
 				const auto index = utils::type_info::index<U>();
+				
 				return GetComponentInfoFromIdx(index);
 			}
 
@@ -83,8 +89,8 @@ namespace gaia {
 			template <typename T>
 			GAIA_NODISCARD bool HasComponentInfo() const {
 				using U = typename DeduceComponent<T>::Type;
-
 				const auto index = utils::type_info::index<U>();
+
 				return m_infoCreateByIndex.find(index) != m_infoCreateByIndex.end();
 			}
 

single_include/gaia.h
@@ -7820,9 +7820,15 @@ namespace gaia {
 				using U = typename DeduceComponent<T>::Type;
 
 				const auto index = utils::type_info::index<U>();
-				(void)m_infoCreateByIndex.try_emplace(index, ComponentInfoCreate::Create<U>());
-				const auto res = m_infoByIndex.try_emplace(index, ComponentInfo::Create<U>());
-				return res.first->second;
+
+				const auto res1 = m_infoCreateByIndex.try_emplace(index, ComponentInfoCreate{});
+				if GAIA_UNLIKELY (res1.second)
+					res1.first->second = ComponentInfoCreate::Create<U>();
+
+				const auto res2 = m_infoByIndex.try_emplace(index, nullptr);
+				if GAIA_UNLIKELY (res2.second)
+					res2.first->second = ComponentInfo::Create<U>();
+				return res2.first->second;
 			}
 
 			template <typename T>
@@ -7837,8 +7843,8 @@ namespace gaia {
 			template <typename T>
 			GAIA_NODISCARD const ComponentInfo* GetComponentInfo() const {
 				using U = typename DeduceComponent<T>::Type;
-
 				const auto index = utils::type_info::index<U>();
+				
 				return GetComponentInfoFromIdx(index);
 			}
 
@@ -7866,8 +7872,8 @@ namespace gaia {
 			template <typename T>
 			GAIA_NODISCARD bool HasComponentInfo() const {
 				using U = typename DeduceComponent<T>::Type;
-
 				const auto index = utils::type_info::index<U>();
+
 				return m_infoCreateByIndex.find(index) != m_infoCreateByIndex.end();
 			}
 