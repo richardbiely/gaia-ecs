3c51e9444d570e5a6432ddd20947fabdcd0c055b
Richard Biely <richardbiely@gmail.com>
2023-05-03 17:34:48 +0200

    Added: Iteration benchmark for 2500 and 5000 archetypes

--- src/perf/iter/src/main.cpp
+++ src/perf/iter/src/main.cpp
@@ -85,46 +85,59 @@ void Create_Archetypes_100(ecs::World& w) {
 	});
 }
 
+template <size_t N, size_t ComponentCount>
+void Create_Archetypes_N(ecs::World& w) {
+	utils::for_each<N>([&](auto i) {
+		AddComponents<bool, i, ComponentCount>(w, EntitiesForArchetype);
+	});
+	//-----------------------------------------
+	utils::for_each<N>([&](auto i) {
+		AddComponents<int8_t, i, ComponentCount>(w, EntitiesForArchetype);
+	});
+	//-----------------------------------------
+	utils::for_each<N>([&](auto i) {
+		AddComponents<uint8_t, i, ComponentCount>(w, EntitiesForArchetype);
+	});
+	//-----------------------------------------
+	utils::for_each<N>([&](auto i) {
+		AddComponents<int16_t, i, ComponentCount>(w, EntitiesForArchetype);
+	});
+	//-----------------------------------------
+	utils::for_each<N>([&](auto i) {
+		AddComponents<uint16_t, i, ComponentCount>(w, EntitiesForArchetype);
+	});
+	//-----------------------------------------
+	utils::for_each<N>([&](auto i) {
+		AddComponents<int32_t, i, ComponentCount>(w, EntitiesForArchetype);
+	});
+	//-----------------------------------------
+	utils::for_each<N>([&](auto i) {
+		AddComponents<uint32_t, i, ComponentCount>(w, EntitiesForArchetype);
+	});
+	//-----------------------------------------
+	utils::for_each<N>([&](auto i) {
+		AddComponents<int64_t, i, ComponentCount>(w, EntitiesForArchetype);
+	});
+	//-----------------------------------------
+	utils::for_each<N>([&](auto i) {
+		AddComponents<uint64_t, i, ComponentCount>(w, EntitiesForArchetype);
+	});
+	//-----------------------------------------
+	utils::for_each<N>([&](auto i) {
+		AddComponents<float, i, ComponentCount>(w, EntitiesForArchetype);
+	});
+}
+
 void Create_Archetypes_1000(ecs::World& w) {
-	utils::for_each<4>([&](auto i) {
-		AddComponents<bool, i, 25>(w, EntitiesForArchetype);
-	});
-	//-----------------------------------------
-	utils::for_each<4>([&](auto i) {
-		AddComponents<int8_t, i, 25>(w, EntitiesForArchetype);
-	});
-	//-----------------------------------------
-	utils::for_each<4>([&](auto i) {
-		AddComponents<uint8_t, i, 25>(w, EntitiesForArchetype);
-	});
-	//-----------------------------------------
-	utils::for_each<4>([&](auto i) {
-		AddComponents<int16_t, i, 25>(w, EntitiesForArchetype);
-	});
-	//-----------------------------------------
-	utils::for_each<4>([&](auto i) {
-		AddComponents<uint16_t, i, 25>(w, EntitiesForArchetype);
-	});
-	//-----------------------------------------
-	utils::for_each<4>([&](auto i) {
-		AddComponents<int32_t, i, 25>(w, EntitiesForArchetype);
-	});
-	//-----------------------------------------
-	utils::for_each<4>([&](auto i) {
-		AddComponents<uint32_t, i, 25>(w, EntitiesForArchetype);
-	});
-	//-----------------------------------------
-	utils::for_each<4>([&](auto i) {
-		AddComponents<int64_t, i, 25>(w, EntitiesForArchetype);
-	});
-	//-----------------------------------------
-	utils::for_each<4>([&](auto i) {
-		AddComponents<uint64_t, i, 25>(w, EntitiesForArchetype);
-	});
-	//-----------------------------------------
-	utils::for_each<4>([&](auto i) {
-		AddComponents<float, i, 25>(w, EntitiesForArchetype);
-	});
+	Create_Archetypes_N<4, 25>(w);
+}
+
+void Create_Archetypes_2500(ecs::World& w) {
+	Create_Archetypes_N<10, 25>(w);
+}
+
+void Create_Archetypes_5000(ecs::World& w) {
+	Create_Archetypes_N<20, 25>(w);
 }
 
 #define DEFINE_FOREACH_INTERNALQUERY(ArchetypeCount)                                                                   \
@@ -147,6 +160,8 @@ void Create_Archetypes_1000(ecs::World& w) {
 DEFINE_FOREACH_INTERNALQUERY(1)
 DEFINE_FOREACH_INTERNALQUERY(100)
 DEFINE_FOREACH_INTERNALQUERY(1000)
+DEFINE_FOREACH_INTERNALQUERY(2500)
+DEFINE_FOREACH_INTERNALQUERY(5000)
 
 #define DEFINE_FOREACH_EXTERNALQUERY(ArchetypeCount)                                                                   \
 	void BM_ForEach_External_##ArchetypeCount(picobench::state& state) {                                                 \
@@ -169,6 +184,8 @@ DEFINE_FOREACH_INTERNALQUERY(1000)
 DEFINE_FOREACH_EXTERNALQUERY(1)
 DEFINE_FOREACH_EXTERNALQUERY(100)
 DEFINE_FOREACH_EXTERNALQUERY(1000)
+DEFINE_FOREACH_EXTERNALQUERY(2500)
+DEFINE_FOREACH_EXTERNALQUERY(5000)
 
 #define DEFINE_FOREACHCHUNK_EXTERNALQUERY(ArchetypeCount)                                                              \
 	void BM_ForEachChunk_External_##ArchetypeCount(picobench::state& state) {                                            \
@@ -193,29 +210,38 @@ DEFINE_FOREACH_EXTERNALQUERY(1000)
 DEFINE_FOREACHCHUNK_EXTERNALQUERY(1)
 DEFINE_FOREACHCHUNK_EXTERNALQUERY(100)
 DEFINE_FOREACHCHUNK_EXTERNALQUERY(1000)
+DEFINE_FOREACHCHUNK_EXTERNALQUERY(2500)
+DEFINE_FOREACHCHUNK_EXTERNALQUERY(5000)
 
-#define PICO_SETTINGS() iterations({8192}).samples(3)
+#define PICO_SETTINGS_CREATEENTITY() iterations({2048}).samples(3)
+#define PICO_SETTINGS_FOREACH() iterations({8192}).samples(3)
 
 PICOBENCH_SUITE("Entity and component creation");
-PICOBENCH(BM_CreateEntity).PICO_SETTINGS().label("0 components");
-PICOBENCH(BM_CreateEntity_With_Component<1>).PICO_SETTINGS().label("1 component");
-PICOBENCH(BM_CreateEntity_With_Component<2>).PICO_SETTINGS().label("2 components");
-PICOBENCH(BM_CreateEntity_With_Component<4>).PICO_SETTINGS().label("4 components");
-PICOBENCH(BM_CreateEntity_With_Component<8>).PICO_SETTINGS().label("8 components");
-PICOBENCH(BM_CreateEntity_With_Component<16>).PICO_SETTINGS().label("16 components");
-PICOBENCH(BM_CreateEntity_With_Component<32>).PICO_SETTINGS().label("32 components");
+PICOBENCH(BM_CreateEntity).PICO_SETTINGS_CREATEENTITY().label("0 components");
+PICOBENCH(BM_CreateEntity_With_Component<1>).PICO_SETTINGS_CREATEENTITY().label("1 component");
+PICOBENCH(BM_CreateEntity_With_Component<2>).PICO_SETTINGS_CREATEENTITY().label("2 components");
+PICOBENCH(BM_CreateEntity_With_Component<4>).PICO_SETTINGS_CREATEENTITY().label("4 components");
+PICOBENCH(BM_CreateEntity_With_Component<8>).PICO_SETTINGS_CREATEENTITY().label("8 components");
+PICOBENCH(BM_CreateEntity_With_Component<16>).PICO_SETTINGS_CREATEENTITY().label("16 components");
+PICOBENCH(BM_CreateEntity_With_Component<32>).PICO_SETTINGS_CREATEENTITY().label("32 components");
 
 PICOBENCH_SUITE("ForEach - internal query");
-PICOBENCH(BM_ForEach_Internal_1).PICO_SETTINGS().label("1 archetype");
-PICOBENCH(BM_ForEach_Internal_100).PICO_SETTINGS().label("100 archetypes");
-PICOBENCH(BM_ForEach_Internal_1000).PICO_SETTINGS().label("1000 archetypes");
+PICOBENCH(BM_ForEach_Internal_1).PICO_SETTINGS_FOREACH().label("1 archetype");
+PICOBENCH(BM_ForEach_Internal_100).PICO_SETTINGS_FOREACH().label("100 archetypes");
+PICOBENCH(BM_ForEach_Internal_1000).PICO_SETTINGS_FOREACH().label("1000 archetypes");
+PICOBENCH(BM_ForEach_Internal_2500).PICO_SETTINGS_FOREACH().label("2500 archetypes");
+PICOBENCH(BM_ForEach_Internal_5000).PICO_SETTINGS_FOREACH().label("5000 archetypes");
 
 PICOBENCH_SUITE("ForEach - external query");
-PICOBENCH(BM_ForEach_External_1).PICO_SETTINGS().label("1 archetype");
-PICOBENCH(BM_ForEach_External_100).PICO_SETTINGS().label("100 archetypes");
-PICOBENCH(BM_ForEach_External_1000).PICO_SETTINGS().label("1000 archetypes");
+PICOBENCH(BM_ForEach_External_1).PICO_SETTINGS_FOREACH().label("1 archetype");
+PICOBENCH(BM_ForEach_External_100).PICO_SETTINGS_FOREACH().label("100 archetypes");
+PICOBENCH(BM_ForEach_External_1000).PICO_SETTINGS_FOREACH().label("1000 archetypes");
+PICOBENCH(BM_ForEach_External_2500).PICO_SETTINGS_FOREACH().label("2500 archetypes");
+PICOBENCH(BM_ForEach_External_5000).PICO_SETTINGS_FOREACH().label("5000 archetypes");
 
 PICOBENCH_SUITE("ForEach - external query, chunk");
-PICOBENCH(BM_ForEachChunk_External_1).PICO_SETTINGS().label("1 archetype");
-PICOBENCH(BM_ForEachChunk_External_100).PICO_SETTINGS().label("100 archetypes");
-PICOBENCH(BM_ForEachChunk_External_1000).PICO_SETTINGS().label("1000 archetypes");
+PICOBENCH(BM_ForEachChunk_External_1).PICO_SETTINGS_FOREACH().label("1 archetype");
+PICOBENCH(BM_ForEachChunk_External_100).PICO_SETTINGS_FOREACH().label("100 archetypes");
+PICOBENCH(BM_ForEachChunk_External_1000).PICO_SETTINGS_FOREACH().label("1000 archetypes");
+PICOBENCH(BM_ForEachChunk_External_2500).PICO_SETTINGS_FOREACH().label("2500 archetypes");
+PICOBENCH(BM_ForEachChunk_External_5000).PICO_SETTINGS_FOREACH().label("5000 archetypes");
