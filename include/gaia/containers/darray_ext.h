c8f26d33c808f284382bedecdaec951644c98720
Richard Biely <richardbiely@gmail.com>
2023-05-04 12:46:23 +0200

    Added: darray_ext which is a hybrid container keeping N items on stack but when crossing the treshold it starts using heap

---
+++ include/gaia/containers/darray_ext.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// TODO: There is no quickly achievable std alternative so go with gaia container
+#include "impl/darray_ext_impl.h"
+
+namespace gaia {
+	namespace containers {
+		template <typename T, auto N>
+		using darray_ext = containers::darr_ext<T, N>;
+	} // namespace containers
+} // namespace gaia
