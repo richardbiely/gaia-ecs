4e1bc9a85083b4d32c07b6572fee8f973ee3d1c5
Richard Biely <richardbiely@gmail.com>
2023-05-04 16:13:58 +0200

    Fixed: QueryId not updated for QueryEntityInfo in 221d566

--- single_include/gaia.h
+++ single_include/gaia.h
@@ -10011,6 +10011,7 @@ namespace gaia {
 				EntityQueryInfo info;
 				query::CalculateMatcherHashes(ctx);
 				info.m_lookupCtx = std::move(ctx);
+				info.m_lookupCtx.queryId = id;
 				return info;
 			}
 
