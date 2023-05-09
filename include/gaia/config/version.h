0ef0a00a616d824646b6b39a4fa04bae446d44f9
Richard Biely <richardbiely@gmail.com>
2023-05-09 18:34:56 +0200

    Changed: Version updated to 0.6

--- include/gaia/config/version.h
+++ include/gaia/config/version.h
@@ -3,6 +3,6 @@
 // Breaking changes and big features
 #define GAIA_VERSION_MAJOR 0
 // Smaller changes and features
-#define GAIA_VERSION_MINOR 5
+#define GAIA_VERSION_MINOR 6
 // Fixes and tweaks
 #define GAIA_VERSION_PATCH 0
