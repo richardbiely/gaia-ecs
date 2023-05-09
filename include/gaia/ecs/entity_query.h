77976a6b693be87d6a6744672bc28d8d6277b3d3
Richard Biely <richardbiely@gmail.com>
2023-05-09 16:31:19 +0200

    Added: Support for query iteration via iterators

--- include/gaia/ecs/entity_query.h
+++ include/gaia/ecs/entity_query.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "../config/config_core_end.h"
+
 #include <tuple>
 #include <type_traits>
 
@@ -569,6 +571,131 @@ namespace gaia {
 
 				RunQueryOnChunks_Internal(queryInfo, [&](Chunk& chunk) {
 					chunk.ForEach(InputArgs{}, func);
+				});
+			}
+
+			struct Iterator {
+			private:
+				query::EntityQueryInfo& m_info;
+				Chunk& m_chunk;
+				uint32_t m_pos;
+
+			public:
+				Iterator(query::EntityQueryInfo& info, Chunk& chunk): m_info(info), m_chunk(chunk), m_pos(0) {}
+				Iterator(query::EntityQueryInfo& info, Chunk& chunk, uint32_t pos): m_info(info), m_chunk(chunk), m_pos(pos) {}
+
+				uint32_t operator*() const {
+					return m_pos;
+				}
+				uint32_t operator->() const {
+					return m_pos;
+				}
+
+				GAIA_NODISCARD Iterator operator[](uint32_t offset) const {
+					return {m_info, m_chunk, m_pos + offset};
+				}
+
+				GAIA_NODISCARD Iterator& operator++() {
+					++m_pos;
+					return *this;
+				}
+				GAIA_NODISCARD Iterator operator++(int) {
+					Iterator temp(*this);
+					++*this;
+					return temp;
+				}
+				GAIA_NODISCARD Iterator& operator--() {
+					--m_pos;
+					return *this;
+				}
+				GAIA_NODISCARD Iterator operator--(int) {
+					Iterator temp(*this);
+					--*this;
+					return temp;
+				}
+
+				GAIA_NODISCARD Iterator operator+(uint32_t offset) const {
+					return {m_info, m_chunk, m_pos + offset};
+				}
+				GAIA_NODISCARD Iterator operator-(uint32_t offset) const {
+					return {m_info, m_chunk, m_pos - offset};
+				}
+				GAIA_NODISCARD uint32_t operator-(const Iterator& other) const {
+					return m_pos - other.m_pos;
+				}
+
+				GAIA_NODISCARD bool operator==(const Iterator& other) const {
+					return m_pos == other.m_pos;
+				}
+				GAIA_NODISCARD bool operator!=(const Iterator& other) const {
+					return m_pos != other.m_pos;
+				}
+				GAIA_NODISCARD bool operator>(const Iterator& other) const {
+					return m_pos > other.m_pos;
+				}
+				GAIA_NODISCARD bool operator>=(const Iterator& other) const {
+					return m_pos >= other.m_pos;
+				}
+				GAIA_NODISCARD bool operator<(const Iterator& other) const {
+					return m_pos < other.m_pos;
+				}
+				GAIA_NODISCARD bool operator<=(const Iterator& other) const {
+					return m_pos <= other.m_pos;
+				}
+
+				GAIA_NODISCARD Iterator begin() const {
+					return *this;
+				}
+
+				GAIA_NODISCARD Iterator end() const {
+					return {m_info, m_chunk, m_chunk.GetEntityCount()};
+				}
+
+				/*!
+				Returns a read-only entity or component view.
+				\warning If \tparam T is a component it is expected it is present. Undefined behavior otherwise.
+				\tparam T Component or Entity
+				\return Entity of component view with read-only access
+				*/
+				template <typename T>
+				GAIA_NODISCARD auto View() const {
+					GAIA_ASSERT(m_info.Has<decltype(m_chunk.View<T>()[0])>());
+					return m_chunk.View<T>();
+				}
+
+				/*!
+				Returns a mutable entity or component view.
+				\warning If \tparam T is a component it is expected it is present. Undefined behavior otherwise.
+				\tparam T Component or Entity
+				\return Entity or component view with read-write access
+				*/
+				template <typename T>
+				GAIA_NODISCARD auto ViewRW() {
+					GAIA_ASSERT(m_info.Has<decltype(m_chunk.ViewRW<T>()[0])>());
+					return m_chunk.ViewRW<T>();
+				}
+
+				/*!
+				Returns a mutable component view.
+				Doesn't update the world version when the access is aquired.
+				\warning It is expected the component \tparam T is present. Undefined behavior otherwise.
+				\tparam T Component
+				\return Component view with read-write access
+				*/
+				template <typename T>
+				GAIA_NODISCARD auto ViewRWSilent() {
+					GAIA_ASSERT(m_info.Has<decltype(m_chunk.ViewRWSilent<T>()[0])>());
+					return m_chunk.ViewRWSilent<T>();
+				}
+			};
+
+			template <typename Func>
+			void ForEachIter(Func func) {
+				auto& queryInfo = FetchQueryInfo();
+
+				RunQueryOnChunks_Internal(queryInfo, [&](Chunk& chunk) {
+					Iterator iter(queryInfo, chunk);
+					func(iter);
 				});
 			}
 
