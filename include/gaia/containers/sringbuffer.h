4a3c2bfad53893620c401bb6e95d2593029e09e9
Richard Biely <richardbiely@gmail.com>
2023-08-20 16:28:23 +0200

    Added: RingBuffer Added: Very simple thread pool

---
+++ include/gaia/containers/sringbuffer.h
@@ -0,0 +1,180 @@
+#pragma once
+#include <cstddef>
+#include <type_traits>
+#include <utility>
+
+#include "../utils/iterator.h"
+#include "../utils/mem.h"
+
+namespace gaia {
+	namespace containers {
+		//! Array of elements of type \tparam T with fixed size and capacity \tparam N allocated on stack.
+		//! Interface compatiblity with std::array where it matters.
+		template <typename T, uint32_t N>
+		class sringbuffer {
+		public:
+			static_assert(N > 1);
+
+			using iterator_category = GAIA_UTIL::random_access_iterator_tag;
+			using value_type = T;
+			using reference = T&;
+			using const_reference = const T&;
+			using pointer = T*;
+			using const_pointer = T*;
+			using difference_type = std::ptrdiff_t;
+			using size_type = decltype(N);
+
+			static constexpr size_type extent = N;
+
+			size_type m_tail{};
+			size_type m_size{};
+			T m_data[N];
+
+			sringbuffer() = default;
+
+			template <typename InputIt>
+			sringbuffer(InputIt first, InputIt last) {
+				const auto count = (size_type)GAIA_UTIL::distance(first, last);
+				GAIA_ASSERT(count <= max_size());
+				if (count < 1)
+					return;
+
+				m_size = count;
+				m_tail = 0;
+
+				if constexpr (std::is_pointer_v<InputIt>) {
+					for (size_type i = 0; i < count; ++i)
+						m_data[i] = first[i];
+				} else if constexpr (std::is_same_v<
+																 typename InputIt::iterator_category, GAIA_UTIL::random_access_iterator_tag>) {
+					for (size_type i = 0; i < count; ++i)
+						m_data[i] = *(first[i]);
+				} else {
+					size_type i = 0;
+					for (auto it = first; it != last; ++it)
+						m_data[i++] = *it;
+				}
+			}
+
+			sringbuffer(std::initializer_list<T> il): sringbuffer(il.begin(), il.end()) {}
+
+			sringbuffer(const sringbuffer& other): sringbuffer(other.begin(), other.end()) {}
+
+			sringbuffer(sringbuffer&& other) noexcept: m_tail(other.m_tail), m_size(other.m_size) {
+				GAIA_ASSERT(GAIA_UTIL::addressof(other) != this);
+
+				utils::transfer_elements(m_data, other.m_data, other.size());
+
+				other.m_tail = size_type(0);
+				other.m_size = size_type(0);
+			}
+
+			GAIA_NODISCARD sringbuffer& operator=(std::initializer_list<T> il) {
+				*this = sringbuffer(il.begin(), il.end());
+				return *this;
+			}
+
+			GAIA_NODISCARD constexpr sringbuffer& operator=(const sringbuffer& other) noexcept {
+				GAIA_ASSERT(GAIA_UTIL::addressof(other) != this);
+
+				utils::copy_elements(m_data, other.m_data, other.size());
+
+				m_tail = other.m_tail;
+				m_size = other.m_size;
+
+				return *this;
+			}
+
+			GAIA_NODISCARD constexpr sringbuffer& operator=(sringbuffer&& other) noexcept {
+				GAIA_ASSERT(GAIA_UTIL::addressof(other) != this);
+
+				utils::transfer_elements(m_data, other.m_data, other.size());
+
+				m_tail = other.m_tail;
+				m_size = other.m_size;
+
+				other.m_tail = size_type(0);
+				other.m_size = size_type(0);
+
+				return *this;
+			}
+
+			~sringbuffer() = default;
+
+			GAIA_NODISCARD void push_back(const T& arg) {
+				GAIA_ASSERT(m_size < N);
+				const auto head = (m_tail + m_size) % N;
+				m_data[head] = arg;
+				++m_size;
+			}
+
+			GAIA_NODISCARD void push_back(T&& arg) {
+				GAIA_ASSERT(m_size < N);
+				const auto head = (m_tail + m_size) % N;
+				m_data[head] = std::forward<T>(arg);
+				++m_size;
+			}
+
+			GAIA_NODISCARD void pop_front(T& out) {
+				GAIA_ASSERT(!empty());
+				out = m_data[m_tail];
+				m_tail = (m_tail + 1) % N;
+				--m_size;
+			}
+
+			GAIA_NODISCARD void pop_front(T&& out) {
+				GAIA_ASSERT(!empty());
+				out = std::forward<T>(m_data[m_tail]);
+				m_tail = (m_tail + 1) % N;
+				--m_size;
+			}
+
+			GAIA_NODISCARD constexpr size_type size() const noexcept {
+				return m_size;
+			}
+
+			GAIA_NODISCARD constexpr bool empty() const noexcept {
+				return !m_size;
+			}
+
+			GAIA_NODISCARD constexpr size_type max_size() const noexcept {
+				return N;
+			}
+
+			GAIA_NODISCARD constexpr reference front() noexcept {
+				GAIA_ASSERT(!empty());
+				return m_data[m_tail];
+			}
+
+			GAIA_NODISCARD constexpr const_reference front() const noexcept {
+				GAIA_ASSERT(!empty());
+				return m_data[m_tail];
+			}
+
+			GAIA_NODISCARD bool operator==(const sringbuffer& other) const {
+				for (size_type i = 0; i < N; ++i)
+					if (!(m_data[i] == other.m_data[i]))
+						return false;
+				return true;
+			}
+		};
+
+		namespace detail {
+			template <typename T, std::size_t N, std::size_t... I>
+			constexpr sringbuffer<std::remove_cv_t<T>, N>
+			to_sringbuffer_impl(T (&a)[N], std::index_sequence<I...> /*no_name*/) {
+				return {{a[I]...}};
+			}
+		} // namespace detail
+
+		template <typename T, std::size_t N>
+		constexpr sringbuffer<std::remove_cv_t<T>, N> to_sringbuffer(T (&a)[N]) {
+			return detail::to_sringbuffer_impl(a, std::make_index_sequence<N>{});
+		}
+
+		template <typename T, typename... U>
+		sringbuffer(T, U...) -> sringbuffer<T, 1 + sizeof...(U)>;
+
+	} // namespace containers
+
+} // namespace gaia
---
+++ include/gaia/mt/threadpool.h
@@ -0,0 +1,213 @@
+#pragma once
+
+#include <atomic>
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <thread>
+
+#include "../containers/sarray_ext.h"
+#include "../containers/sringbuffer.h"
+
+namespace gaia {
+	namespace mt {
+		struct Job {
+			std::function<void()> func;
+		};
+
+		struct JobArgs {
+			uint32_t jobIndex;
+			uint32_t idxStart;
+			uint32_t idxEnd;
+		};
+
+		struct JobParallel {
+			std::function<void(const JobArgs&)> func;
+		};
+
+		class JobQueue {
+			std::mutex m_jobsLock;
+			containers::sringbuffer<Job, 512> m_jobs;
+
+		public:
+			//! Tries adding a job to the queue.
+			//! \return True if the job was added. False if it could not be added (e.g. maximum capacity has been reached).
+			bool Push(const Job& job) {
+				std::lock_guard<std::mutex> guard(m_jobsLock);
+				if (m_jobs.size() >= m_jobs.max_size())
+					return false;
+
+				m_jobs.push_back(job);
+				return true;
+			}
+
+			//! Tries retriving a job to the queue.
+			//! \return True if the job was retrived. False if it could not be retrived (e.g. there are no jobs).
+			bool Pop(Job& job) {
+				std::lock_guard<std::mutex> guard(m_jobsLock);
+				if (m_jobs.empty())
+					return false;
+				m_jobs.pop_front(std::move(job));
+				return true;
+			}
+		};
+
+		class ThreadPool final {
+			containers::sarr_ext<std::thread, 64> m_workers;
+
+			//! List of pending jobs
+			JobQueue m_jobs;
+			//! State of execution of the main thread.
+			//! So long it is greater than m_workersCounter it means workers are doing work.
+			std::uint32_t m_mainCounter;
+			//! State of execution of worker threads
+			std::atomic_uint32_t m_workersCounter;
+
+			std::mutex m_cvLock;
+			std::condition_variable m_cv;
+
+			//! When true the pool is supposed to finish all work and terminate all threads
+			bool m_stop;
+
+		private:
+			ThreadPool(): m_stop(false), m_mainCounter(0), m_workersCounter(0) {
+				uint32_t workerCount = CalculateThreadCount(0);
+				if (workerCount > m_workers.max_size())
+					workerCount = m_workers.max_size();
+
+				m_workers.resize(workerCount);
+				for (uint32_t i = 0; i < workerCount; ++i) {
+					m_workers[i] = std::thread([this]() {
+						ThreadFunc(m_stop);
+					});
+				}
+			}
+
+			ThreadPool(ThreadPool&&) = delete;
+			ThreadPool(const ThreadPool&) = delete;
+			ThreadPool& operator=(ThreadPool&&) = delete;
+			ThreadPool& operator=(const ThreadPool&) = delete;
+
+			static uint32_t CalculateThreadCount(uint32_t threadsWanted) {
+				// Make sure a reasonable amount of threads is used
+				if (threadsWanted == 0) {
+					// Subtract one (the main thread)
+					threadsWanted = std::thread::hardware_concurrency() - 1;
+					if (threadsWanted <= 0)
+						threadsWanted = 1;
+				}
+				return threadsWanted;
+			}
+
+			void ThreadFunc(const bool& stop) {
+				Job job;
+				while (!stop) {
+					if (m_jobs.Pop(job)) {
+						job.func();
+						++m_workersCounter;
+					} else {
+						std::unique_lock<std::mutex> lock(m_cvLock);
+						m_cv.wait(lock);
+					}
+				}
+			}
+
+		public:
+			static ThreadPool& Get() {
+				static ThreadPool threadPool;
+				return threadPool;
+			}
+
+			~ThreadPool() {
+				// Request stopping
+				m_stop = true;
+				// Complete all remaining work
+				CompleteAll();
+				// Wake up any threads that were put to sleep
+				m_cv.notify_all();
+				// Join threads with the main one
+				for (auto& w: m_workers)
+					w.join();
+			}
+
+			//! Schedules a job to run on a worker thread.
+			//! \warning Must be used form the main thread.
+			void Schedule(const Job& job) {
+				// Don't add new jobs once stop was requested
+				if GAIA_UNLIKELY (m_stop)
+					return;
+
+				++m_mainCounter;
+
+				// Try pushing a new job until it we succeed.
+				// The thread is put to sleep if pushing the jobs fails.
+				while (!m_jobs.Push(job))
+					Poll();
+
+				// Wake one worker thread
+				m_cv.notify_one();
+			}
+
+			//! Schedules a job to run on worker threads in parallel.
+			//! \warning Must be used form the main thread.
+			void ScheduleParallel(const JobParallel& job, uint32_t itemsToProcess, uint32_t groupSize) {
+				// Empty data set or no group size specified are considered wrong inputs
+				GAIA_ASSERT(itemsToProcess != 0 && groupSize != 0);
+				if (itemsToProcess == 0 || groupSize == 0)
+					return;
+
+				// Don't add new jobs once stop was requested
+				if GAIA_UNLIKELY (m_stop)
+					return;
+
+				const auto jobs = (itemsToProcess + groupSize - 1) / groupSize;
+				m_mainCounter += jobs;
+
+				for (uint32_t jobIndex = 0; jobIndex < jobs; ++jobIndex) {
+					// Create one job per group
+					auto groupJob = [job, itemsToProcess, groupSize, jobIndex]() {
+						const uint32_t groupJobIdxStart = jobIndex * groupSize;
+						const uint32_t groupJobIdxEnd = std::min(groupJobIdxStart + groupSize, itemsToProcess);
+
+						JobArgs args;
+						args.jobIndex = jobIndex;
+						args.idxStart = groupJobIdxStart;
+						args.idxEnd = groupJobIdxEnd;
+						job.func(args);
+					};
+
+					// Try to pushing a new job until it we succeed.
+					// The thread is put to sleep if pushing the jobs fails.
+					while (!m_jobs.Push({groupJob}))
+						Poll();
+
+					// Wake some worker thread
+					m_cv.notify_one();
+				}
+			}
+
+			//! Wait for all jobs to finish.
+			//! \warning Must be used form the main thread.
+			void CompleteAll() {
+				while (IsBusy())
+					Poll();
+			}
+
+		private:
+			//! Checks whether workers are busy doing work.
+			//!	\return True if any workers are busy doing work.
+			bool IsBusy() const {
+				return m_workersCounter.load() < m_mainCounter;
+			}
+
+			//! Wakes up some worker thread and reschedules the current one.
+			void Poll() {
+				// Wake some worker thread
+				m_cv.notify_one();
+
+				// Allow this thread to be rescheduled
+				std::this_thread::yield();
+			}
+		};
+	} // namespace mt
+} // namespace gaia
