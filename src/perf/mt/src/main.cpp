65035e7d7d3c4b987893bc245850cde4cf076db9
Richard Biely <richardbiely@gmail.com>
2023-08-24 10:45:51 +0200

    Fixed: Build issues

--- src/perf/mt/src/main.cpp
+++ src/perf/mt/src/main.cpp
@@ -27,6 +27,28 @@ static uint32_t BenchFunc_Complex(std::span<const uint32_t> arr) {
 	return sum;
 }
 
+void Run_Schedule_Empty(uint32_t Jobs) {
+	auto& tp = mt::ThreadPool::Get();
+
+	for (uint32_t i = 0; i < Jobs; i++) {
+		mt::Job job;
+		job.func = []() {};
+		tp.Schedule(job);
+	}
+	tp.CompleteAll();
+}
+
+void BM_Schedule_Empty(picobench::state& state) {
+	const auto user_data = state.user_data();
+	const uint32_t N = user_data & 0xFFFFFFFF;
+	const uint32_t Jobs = N;
+
+	for (auto _: state) {
+		(void)_;
+		Run_Schedule_Empty(Jobs);
+	}
+}
+
 template <typename Func>
 void Run_Schedule_Simple(const uint32_t* pArr, uint32_t Jobs, uint32_t ItemsPerJob, Func func) {
 	auto& tp = mt::ThreadPool::Get();
@@ -53,8 +75,6 @@ void BM_Schedule_Simple(picobench::state& state) {
 	const uint32_t Jobs = user_data >> 32;
 	const uint32_t ItemsPerJob = N / Jobs;
 
-	auto& tp = mt::ThreadPool::Get();
-
 	containers::darray<uint32_t> arr;
 	arr.resize(N);
 	for (uint32_t i = 0; i < arr.size(); ++i)
@@ -71,8 +91,6 @@ void BM_Schedule_Complex(picobench::state& state) {
 	const uint32_t N = user_data & 0xFFFFFFFF;
 	const uint32_t Jobs = user_data >> 32;
 	const uint32_t ItemsPerJob = N / Jobs;
-
-	auto& tp = mt::ThreadPool::Get();
 
 	containers::darray<uint32_t> arr;
 	arr.resize(N);
@@ -106,8 +124,6 @@ void BM_ScheduleParallel_Simple(picobench::state& state) {
 	const auto user_data = state.user_data();
 	const uint32_t N = user_data & 0xFFFFFFFF;
 
-	auto& tp = mt::ThreadPool::Get();
-
 	containers::darray<uint32_t> arr;
 	arr.resize(N);
 	for (uint32_t i = 0; i < arr.size(); ++i)
@@ -122,8 +138,6 @@ void BM_ScheduleParallel_Simple(picobench::state& state) {
 void BM_ScheduleParallel_Complex(picobench::state& state) {
 	const auto user_data = state.user_data();
 	const uint32_t N = user_data & 0xFFFFFFFF;
-
-	auto& tp = mt::ThreadPool::Get();
 
 	containers::darray<uint32_t> arr;
 	arr.resize(N);
@@ -146,18 +160,26 @@ void BM_ScheduleParallel_Complex(picobench::state& state) {
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
+// Measures job creation overhead.
+////////////////////////////////////////////////////////////////////////////////////////////////
+PICOBENCH_SUITE("Schedule - Empty");
+PICOBENCH(BM_Schedule_Empty).PICO_SETTINGS().user_data(1000).label("Schedule, 1000");
+PICOBENCH(BM_Schedule_Empty).PICO_SETTINGS().user_data(5000).label("Schedule, 5000");
+PICOBENCH(BM_Schedule_Empty).PICO_SETTINGS().user_data(10000).label("Schedule, 10000");
+
+////////////////////////////////////////////////////////////////////////////////////////////////
 // Low load most likely to show scheduling overhead.
 ////////////////////////////////////////////////////////////////////////////////////////////////
 static constexpr uint32_t ItemsToProcess_Trivial = 1'000;
 PICOBENCH_SUITE("Schedule/ScheduleParallel - Trivial");
-PICOBENCH(BM_Schedule_Simple).PICO_SETTINGS().user_data(ItemsToProcess_Trivial | (1ll << 32)).label("Schedule, 1T");
-PICOBENCH(BM_Schedule_Simple).PICO_SETTINGS().user_data(ItemsToProcess_Trivial | (2ll << 32)).label("Schedule, 2T");
-PICOBENCH(BM_Schedule_Simple).PICO_SETTINGS().user_data(ItemsToProcess_Trivial | (4ll << 32)).label("Schedule, 4T");
-PICOBENCH(BM_Schedule_Simple).PICO_SETTINGS().user_data(ItemsToProcess_Trivial | (8ll << 32)).label("Schedule, 8T");
+PICOBENCH(BM_Schedule_Simple).PICO_SETTINGS().user_data(ItemsToProcess_Trivial | (1ll << 32)).label("Schedule, 1");
+PICOBENCH(BM_Schedule_Simple).PICO_SETTINGS().user_data(ItemsToProcess_Trivial | (2ll << 32)).label("Schedule, 2");
+PICOBENCH(BM_Schedule_Simple).PICO_SETTINGS().user_data(ItemsToProcess_Trivial | (4ll << 32)).label("Schedule, 4");
+PICOBENCH(BM_Schedule_Simple).PICO_SETTINGS().user_data(ItemsToProcess_Trivial | (8ll << 32)).label("Schedule, 8");
 PICOBENCH(BM_Schedule_Simple)
 		.PICO_SETTINGS()
 		.user_data(ItemsToProcess_Trivial | ((uint64_t)mt::ThreadPool::Get().GetWorkersCount()) << 32)
-		.label("Schedule, all workers");
+		.label("Schedule, MAX");
 PICOBENCH(BM_ScheduleParallel_Simple).PICO_SETTINGS().user_data(ItemsToProcess_Trivial).label("ScheduleParallel");
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -165,14 +187,14 @@ PICOBENCH(BM_ScheduleParallel_Simple).PICO_SETTINGS().user_data(ItemsToProcess_T
 ////////////////////////////////////////////////////////////////////////////////////////////////
 static constexpr uint32_t ItemsToProcess_Simple = 1'000'000;
 PICOBENCH_SUITE("Schedule/ScheduleParallel - Simple");
-PICOBENCH(BM_Schedule_Simple).PICO_SETTINGS().user_data(ItemsToProcess_Simple | (1ll << 32)).label("Schedule, 1T");
-PICOBENCH(BM_Schedule_Simple).PICO_SETTINGS().user_data(ItemsToProcess_Simple | (2ll << 32)).label("Schedule, 2T");
-PICOBENCH(BM_Schedule_Simple).PICO_SETTINGS().user_data(ItemsToProcess_Simple | (4ll << 32)).label("Schedule, 4T");
-PICOBENCH(BM_Schedule_Simple).PICO_SETTINGS().user_data(ItemsToProcess_Simple | (8ll << 32)).label("Schedule, 8T");
+PICOBENCH(BM_Schedule_Simple).PICO_SETTINGS().user_data(ItemsToProcess_Simple | (1ll << 32)).label("Schedule, 1");
+PICOBENCH(BM_Schedule_Simple).PICO_SETTINGS().user_data(ItemsToProcess_Simple | (2ll << 32)).label("Schedule, 2");
+PICOBENCH(BM_Schedule_Simple).PICO_SETTINGS().user_data(ItemsToProcess_Simple | (4ll << 32)).label("Schedule, 4");
+PICOBENCH(BM_Schedule_Simple).PICO_SETTINGS().user_data(ItemsToProcess_Simple | (8ll << 32)).label("Schedule, 8");
 PICOBENCH(BM_Schedule_Simple)
 		.PICO_SETTINGS()
 		.user_data(ItemsToProcess_Simple | ((uint64_t)mt::ThreadPool::Get().GetWorkersCount()) << 32)
-		.label("Schedule, all workers");
+		.label("Schedule, MAX");
 PICOBENCH(BM_ScheduleParallel_Simple).PICO_SETTINGS().user_data(ItemsToProcess_Simple).label("ScheduleParallel");
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -180,12 +202,12 @@ PICOBENCH(BM_ScheduleParallel_Simple).PICO_SETTINGS().user_data(ItemsToProcess_S
 ////////////////////////////////////////////////////////////////////////////////////////////////
 static constexpr uint32_t ItemsToProcess_Complex = 1'000'000;
 PICOBENCH_SUITE("Schedule/ScheduleParallel - Complex");
-PICOBENCH(BM_Schedule_Complex).PICO_SETTINGS().user_data(ItemsToProcess_Complex | (1ll << 32)).label("Schedule, 1T");
-PICOBENCH(BM_Schedule_Complex).PICO_SETTINGS().user_data(ItemsToProcess_Complex | (2ll << 32)).label("Schedule, 2T");
-PICOBENCH(BM_Schedule_Complex).PICO_SETTINGS().user_data(ItemsToProcess_Complex | (4ll << 32)).label("Schedule, 4T");
-PICOBENCH(BM_Schedule_Complex).PICO_SETTINGS().user_data(ItemsToProcess_Complex | (8ll << 32)).label("Schedule, 8T");
+PICOBENCH(BM_Schedule_Complex).PICO_SETTINGS().user_data(ItemsToProcess_Complex | (1ll << 32)).label("Schedule, 1");
+PICOBENCH(BM_Schedule_Complex).PICO_SETTINGS().user_data(ItemsToProcess_Complex | (2ll << 32)).label("Schedule, 2");
+PICOBENCH(BM_Schedule_Complex).PICO_SETTINGS().user_data(ItemsToProcess_Complex | (4ll << 32)).label("Schedule, 4");
+PICOBENCH(BM_Schedule_Complex).PICO_SETTINGS().user_data(ItemsToProcess_Complex | (8ll << 32)).label("Schedule, 8");
 PICOBENCH(BM_Schedule_Complex)
 		.PICO_SETTINGS()
 		.user_data(ItemsToProcess_Complex | ((uint64_t)mt::ThreadPool::Get().GetWorkersCount()) << 32)
-		.label("Schedule, all workers");
+		.label("Schedule, MAX");
 PICOBENCH(BM_ScheduleParallel_Complex).PICO_SETTINGS().user_data(ItemsToProcess_Complex).label("ScheduleParallel");
