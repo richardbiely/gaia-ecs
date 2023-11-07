af49471fa04587eb976048ea901eac24539df3ed
Richard Biely <richardbiely@gmail.com>
2023-11-07 16:36:11 +0100

    Changed: Quom installed into a virtual pyhton environment if not previously available

--- make_single_header.sh
+++ make_single_header.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
 PATH_TO_QUOM_DIR="${@:1}"
+PATH_TO_PIP_EVN="${PATH_TO_QUOM_DIR}/../../../pip_venv"
+PATH_TO_QUOM=$(which quom)
 
-# Install quom via python script if necessary.
-# This requires pip to be available.
-HAS_QUOM=$(which quom)
-if [ -z "$HAS_QUOM" ]; then
-  echo "Installing quim to ${PATH_TO_QUOM_DIR}"
-  pip install "${PATH_TO_QUOM_DIR}"
+# If quom is not found right away, try searching in the virtual environment
+if [ -z "$PATH_TO_QUOM" ]; then
+  PATH_TO_QUOM=$(which "${PATH_TO_PIP_EVN}/bin/quom")
+fi
+
+if [ -z "${PATH_TO_QUOM}" ]; then
+  PATH_TO_QUOM="${PATH_TO_PIP_EVN}/bin/quom"
+
+  # Still not found. Try installing quom into the virtual environment.
+  # This requires python3 to be available.
+  echo "Creating virtual environment for quom to ${PATH_TO_PIP_EVN}"
+  python3 -m venv "${PATH_TO_PIP_EVN}"
+  echo "Installing quom to ${PATH_TO_PIP_EVN}/bin"
+  "${PATH_TO_PIP_EVN}/bin/pip" install "${PATH_TO_QUOM_DIR}"
+  chmod +x "${PATH_TO_QUOM}"
 fi
 
 # This turns Gaia-ECS into a single header library by generating "gaia.h"
 # which you can simply drop into your project and start using.
 echo "Generating ./single_include/gaia.h"
-quom ./include/gaia.h ./single_include/gaia.h -I ./include
+${PATH_TO_QUOM} ./include/gaia.h ./single_include/gaia.h -I ./include
