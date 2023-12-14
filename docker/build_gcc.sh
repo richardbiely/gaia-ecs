79567c5de7add586f1d8fa11b5a509ff51ee62e4
Richard Biely <richardbiely@gmail.com>
2023-12-14 14:30:19 +0100

    Fixed: build_gcc.sh would invoke an old version of gcc

--- docker/build_gcc.sh
+++ docker/build_gcc.sh
@@ -18,8 +18,8 @@ mkdir ${PATH_BASE} -p
 # Compiler
 ####################################################################
 
-export CC="/usr/bin/gcc-7"
-export CXX="/usr/bin/g++-7"
+export CC="/usr/bin/gcc"
+export CXX="/usr/bin/g++"
 
 ####################################################################
 # Build the project
