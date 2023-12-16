639afdaa3ba98c3efdc565001a8a620bb4c77323
Richard Biely <richardbiely@gmail.com>
2023-12-16 07:08:37 +0100

    Changed: Version bump to 0.8.3

--- single_include/gaia.h
+++ single_include/gaia.h
@@ -606,7 +606,7 @@ namespace gaia {
 // Smaller changes and features
 #define GAIA_VERSION_MINOR 8
 // Fixes and tweaks
-#define GAIA_VERSION_PATCH 2
+#define GAIA_VERSION_PATCH 3
 
 //------------------------------------------------------------------------------
 // General settings.
