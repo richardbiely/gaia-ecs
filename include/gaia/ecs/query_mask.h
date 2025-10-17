14ba417c3f6cdd89f57b7e00c86ac21678ef8d16
Richard Biely <richardbiely@gmail.com>
2025-10-17 17:17:43 +0200

    Tweaked: Minor query lookup performance improvement

--- include/gaia/ecs/query_mask.h
+++ include/gaia/ecs/query_mask.h
@@ -20,10 +20,10 @@ namespace gaia {
 			uint64_t value[4];
 
 			bool operator==(const QueryMask& other) const {
-				return value[0] == other.value[0] && //
-							 value[1] == other.value[1] && //
-							 value[2] == other.value[2] && //
-							 value[3] == other.value[3];
+				return ((value[0] ^ other.value[0]) | //
+								(value[1] ^ other.value[1]) | //
+								(value[2] ^ other.value[2]) | //
+								(value[3] ^ other.value[3])) == 0;
 			}
 			bool operator!=(const QueryMask& other) const {
 				return !(*this == other);
@@ -33,10 +33,21 @@ namespace gaia {
 		//! Hash an entity id into one bit per 64-bit block
 		GAIA_NODISCARD inline QueryMask hash_entity_id(Entity entity) {
 			QueryMask mask{};
-			for (uint32_t i = 0; i < 4; ++i) {
-				const uint64_t bit = (entity.id() * s_ct_queryMask_primes[i]) >> (64 - 4); // pick 1 bit in each 16-bit block
-				mask.value[i] = (1ull << bit);
-			}
+			const uint64_t id = entity.id();
+
+			// Pick 1 bit in each 16-bit block.
+			// We are shifting right by 60 bits, leaving only the top 4 bits of the 64-bit product. This effectively
+			// gives us a random-ish value in the range 0–15, which fits one bit per 16-bit block (since 2^4 = 16).
+			const auto bit0 = (id * s_ct_queryMask_primes[0]) >> (64 - 4);
+			const auto bit1 = (id * s_ct_queryMask_primes[1]) >> (64 - 4);
+			const auto bit2 = (id * s_ct_queryMask_primes[2]) >> (64 - 4);
+			const auto bit3 = (id * s_ct_queryMask_primes[3]) >> (64 - 4);
+
+			mask.value[0] = 1ull << bit0;
+			mask.value[1] = 1ull << bit1;
+			mask.value[2] = 1ull << bit2;
+			mask.value[3] = 1ull << bit3;
+
 			return mask;
 		}
 
@@ -45,18 +56,21 @@ namespace gaia {
 			QueryMask result{};
 			for (auto entity: entities) {
 				QueryMask hash = hash_entity_id(entity);
-				for (uint32_t i = 0; i < 4; ++i)
-					result.value[i] |= hash.value[i];
+				result.value[0] |= hash.value[0];
+				result.value[1] |= hash.value[1];
+				result.value[2] |= hash.value[2];
+				result.value[3] |= hash.value[3];
 			}
 			return result;
 		}
 
 		//! Checks is there is a match between two masks
 		GAIA_NODISCARD inline bool match_entity_mask(const QueryMask& m1, const QueryMask& m2) {
-			return (m1.value[0] & m2.value[0]) != 0 && //
-						 (m1.value[1] & m2.value[1]) != 0 && //
-						 (m1.value[2] & m2.value[2]) != 0 && //
-						 (m1.value[3] & m2.value[3]) != 0;
+			const auto r0 = m1.value[0] & m2.value[0];
+			const auto r1 = m1.value[1] & m2.value[1];
+			const auto r2 = m1.value[2] & m2.value[2];
+			const auto r3 = m1.value[3] & m2.value[3];
+			return (r0 | r1 | r2 | r3) != 0;
 		}
 #else
 		using QueryMask = uint64_t;
