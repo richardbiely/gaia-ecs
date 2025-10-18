richardbiely/gaia-ecs

commit bce5769092944ecb262bd7a6f5fb44e0b4196225

Added: zero-initialization constructor for containers Fixed: Undefined behavior of empty container comparisons when unit testing

include/gaia/cnt/impl/darray_ext_impl.h
@@ -181,7 +181,23 @@ namespace gaia {
 			}
 
 		public:
-			darr_ext() noexcept = default;
+			darr_ext() noexcept {
+				if constexpr (!mem::is_soa_layout_v<T>)
+					core::call_ctor_raw_n(data(), extent);
+			}
+
+			//! Zero-initialization constructor. Because darr_ext is not aggretate type, doing: darr_ext<int,10> tmp{} does
+			//! not zero-initialize its internals. We need to be explicit about our intent and use a special constructor.
+			constexpr darr_ext(core::zero_t) noexcept
+			{
+				// explicit zeroing
+				if constexpr (!mem::is_soa_layout_v<T>)
+					core::call_ctor_n(data(), extent);
+				else {
+					for (auto i = (size_type)0; i < N; ++i)
+						operator[](i) = {};
+				}
+			}
 
 			darr_ext(size_type count, const T& value) {
 				resize(count);

include/gaia/cnt/impl/sarray_ext_impl.h
@@ -147,6 +147,18 @@ namespace gaia {
 					core::call_ctor_raw_n(data(), extent);
 			}
 
+			//! Zero-initialization constructor. Because sarr_ext is not aggretate type, doing: sarr_ext<int,10> tmp{} does
+			//! not zero-initialize its internals. We need to be explicit about our intent and use a special constructor.
+			constexpr sarr_ext(core::zero_t) noexcept {
+				// explicit zeroing
+				if constexpr (!mem::is_soa_layout_v<T>)
+					core::call_ctor_n(data(), extent);
+				else {
+					for (auto i = (size_type)0; i < N; ++i)
+						operator[](i) = {};
+				}
+			}
+
 			~sarr_ext() {
 				if constexpr (!mem::is_soa_layout_v<T>)
 					core::call_dtor_n(data(), m_cnt);

include/gaia/cnt/impl/sarray_impl.h
@@ -142,6 +142,19 @@ namespace gaia {
 				if constexpr (!mem::is_soa_layout_v<T>)
 					core::call_ctor_raw_n(data(), extent);
 			}
+			
+
+			//! Zero-initialization constructor. Because sarr is not aggretate type, doing: sarr<int,10> tmp{} does not
+			//! zero-initialize its internals. We need to be explicit about our intent and use a special constructor.
+			constexpr sarr(core::zero_t) noexcept {
+				// explicit zeroing
+				if constexpr (!mem::is_soa_layout_v<T>)
+					core::call_ctor_n(data(), extent);
+				else {
+					for (auto i = (size_type)0; i < N; ++i)
+						operator[](i) = {};
+				}
+			}
 
 			~sarr() {
 				if constexpr (!mem::is_soa_layout_v<T>)

single_include/gaia.h
@@ -1492,6 +1492,11 @@ namespace gaia {
 			}
 		} // namespace detail
 
+		struct zero_t {
+			explicit constexpr zero_t() = default;
+		};
+		inline constexpr zero_t zero{};
+
 		template <class T>
 		using rem_rp_t = typename detail::rem_rp<T>::type;
 
@@ -1659,19 +1664,15 @@ namespace gaia {
 		template <typename T>
 		void call_ctor(T* pData) {
 			GAIA_ASSERT(pData != nullptr);
-			if constexpr (!std::is_trivially_constructible_v<T>) {
-				(void)::new (pData) T();
-			}
+			(void)::new (pData) T();
 		}
 
 		//! Constructs \param cnt objects of type \tparam T starting at the memory address \param pData.
 		template <typename T>
 		void call_ctor_n(T* pData, size_t cnt) {
 			GAIA_ASSERT(pData != nullptr);
-			if constexpr (!std::is_trivially_constructible_v<T>) {
-				for (size_t i = 0; i < cnt; ++i)
-					(void)::new (pData + i) T();
-			}
+			for (size_t i = 0; i < cnt; ++i)
+				(void)::new (pData + i) T();
 		}
 
 		template <typename T, typename... Args>
@@ -1687,19 +1688,15 @@ namespace gaia {
 		template <typename T>
 		void call_dtor(T* pData) {
 			GAIA_ASSERT(pData != nullptr);
-			if constexpr (!std::is_trivially_destructible_v<T>) {
-				pData->~T();
-			}
+			pData->~T();
 		}
 
 		//! Constructs \param cnt objects of type \tparam T starting at the memory address \param pData.
 		template <typename T>
 		void call_dtor_n(T* pData, size_t cnt) {
 			GAIA_ASSERT(pData != nullptr);
-			if constexpr (!std::is_trivially_destructible_v<T>) {
-				for (size_t i = 0; i < cnt; ++i)
-					pData[i].~T();
-			}
+			for (size_t i = 0; i < cnt; ++i)
+				pData[i].~T();
 		}
 
 		//----------------------------------------------------------------------
@@ -6127,6 +6124,7 @@ namespace gaia {
 
 		public:
 			constexpr darr() noexcept = default;
+			constexpr darr(core::zero_t) noexcept {}
 
 			darr(size_type count, const_reference value) {
 				resize(count);
@@ -6814,7 +6812,23 @@ namespace gaia {
 			}
 
 		public:
-			darr_ext() noexcept = default;
+			darr_ext() noexcept {
+				if constexpr (!mem::is_soa_layout_v<T>)
+					core::call_ctor_raw_n(data(), extent);
+			}
+
+			//! Zero-initialization constructor. Because darr_ext is not aggretate type, doing: darr_ext<int,10> tmp{} does
+			//! not zero-initialize its internals. We need to be explicit about our intent and use a special constructor.
+			constexpr darr_ext(core::zero_t) noexcept
+			{
+				// explicit zeroing
+				if constexpr (!mem::is_soa_layout_v<T>)
+					core::call_ctor_n(data(), extent);
+				else {
+					for (auto i = (size_type)0; i < N; ++i)
+						operator[](i) = {};
+				}
+			}
 
 			darr_ext(size_type count, const T& value) {
 				resize(count);
@@ -11098,6 +11112,18 @@ namespace gaia {
 					core::call_ctor_raw_n(data(), extent);
 			}
 
+			//! Zero-initialization constructor. Because sarr is not aggretate type, doing: sarr<int,10> tmp{} does not
+			//! zero-initialize its internals. We need to be explicit about our intent and use a special constructor.
+			constexpr sarr(core::zero_t) noexcept {
+				// explicit zeroing
+				if constexpr (!mem::is_soa_layout_v<T>)
+					core::call_ctor_n(data(), extent);
+				else {
+					for (auto i = (size_type)0; i < N; ++i)
+						operator[](i) = {};
+				}
+			}
+
 			~sarr() {
 				if constexpr (!mem::is_soa_layout_v<T>)
 					core::call_dtor_n(data(), extent);
@@ -12798,6 +12824,18 @@ namespace gaia {
 			constexpr sarr_ext() noexcept {
 				if constexpr (!mem::is_soa_layout_v<T>)
 					core::call_ctor_raw_n(data(), extent);
+			}
+
+			//! Zero-initialization constructor. Because sarr_ext is not aggretate type, doing: sarr_ext<int,10> tmp{} does
+			//! not zero-initialize its internals. We need to be explicit about our intent and use a special constructor.
+			constexpr sarr_ext(core::zero_t) noexcept {
+				// explicit zeroing
+				if constexpr (!mem::is_soa_layout_v<T>)
+					core::call_ctor_n(data(), extent);
+				else {
+					for (auto i = (size_type)0; i < N; ++i)
+						operator[](i) = {};
+				}
 			}
 
 			~sarr_ext() {

include/gaia/cnt/impl/darray_impl.h
@@ -164,6 +164,7 @@ namespace gaia {
 
 		public:
 			constexpr darr() noexcept = default;
+			constexpr darr(core::zero_t) noexcept {}
 
 			darr(size_type count, const_reference value) {
 				resize(count);

include/gaia/core/utility.h
@@ -81,6 +81,11 @@ namespace gaia {
 			}
 		} // namespace detail
 
+		struct zero_t {
+			explicit constexpr zero_t() = default;
+		};
+		inline constexpr zero_t zero{};
+
 		template <class T>
 		using rem_rp_t = typename detail::rem_rp<T>::type;
 
@@ -248,19 +253,15 @@ namespace gaia {
 		template <typename T>
 		void call_ctor(T* pData) {
 			GAIA_ASSERT(pData != nullptr);
-			if constexpr (!std::is_trivially_constructible_v<T>) {
-				(void)::new (pData) T();
-			}
+			(void)::new (pData) T();
 		}
 
 		//! Constructs \param cnt objects of type \tparam T starting at the memory address \param pData.
 		template <typename T>
 		void call_ctor_n(T* pData, size_t cnt) {
 			GAIA_ASSERT(pData != nullptr);
-			if constexpr (!std::is_trivially_constructible_v<T>) {
-				for (size_t i = 0; i < cnt; ++i)
-					(void)::new (pData + i) T();
-			}
+			for (size_t i = 0; i < cnt; ++i)
+				(void)::new (pData + i) T();
 		}
 
 		template <typename T, typename... Args>
@@ -276,19 +277,15 @@ namespace gaia {
 		template <typename T>
 		void call_dtor(T* pData) {
 			GAIA_ASSERT(pData != nullptr);
-			if constexpr (!std::is_trivially_destructible_v<T>) {
-				pData->~T();
-			}
+			pData->~T();
 		}
 
 		//! Constructs \param cnt objects of type \tparam T starting at the memory address \param pData.
 		template <typename T>
 		void call_dtor_n(T* pData, size_t cnt) {
 			GAIA_ASSERT(pData != nullptr);
-			if constexpr (!std::is_trivially_destructible_v<T>) {
-				for (size_t i = 0; i < cnt; ++i)
-					pData[i].~T();
-			}
+			for (size_t i = 0; i < cnt; ++i)
+				pData[i].~T();
 		}
 
 		//----------------------------------------------------------------------

src/test/src/main.cpp
@@ -583,7 +583,7 @@ void fixed_arr_test() {
 
 	// Container comparison
 	{
-		Container arrEmpty;
+		Container arrEmpty(core::zero);
 		CHECK_FALSE(arrEmpty == arr);
 
 		Container arr2(arr);
@@ -664,7 +664,7 @@ void resizable_arr_test(uint32_t N) {
 
 	// Container comparison
 	{
-		Container arrEmpty;
+		Container arrEmpty(core::zero);
 		CHECK_FALSE(arrEmpty == arr);
 
 		Container arr2(arr);

commit db05eefb3af65a38d73560419394cc711dd23591

Fixed: Regression in bce5769 Fixed: sarr_ext deafult constructor does not need to initialize its storage (size=0 by default)

include/gaia/cnt/impl/darray_ext_impl.h
@@ -181,23 +181,8 @@ namespace gaia {
 			}
 
 		public:
-			darr_ext() noexcept {
-				if constexpr (!mem::is_soa_layout_v<T>)
-					core::call_ctor_raw_n(data(), extent);
-			}
-
-			//! Zero-initialization constructor. Because darr_ext is not aggretate type, doing: darr_ext<int,10> tmp{} does
-			//! not zero-initialize its internals. We need to be explicit about our intent and use a special constructor.
-			constexpr darr_ext(core::zero_t) noexcept
-			{
-				// explicit zeroing
-				if constexpr (!mem::is_soa_layout_v<T>)
-					core::call_ctor_n(data(), extent);
-				else {
-					for (auto i = (size_type)0; i < N; ++i)
-						operator[](i) = {};
-				}
-			}
+			darr_ext() noexcept = default;
+			constexpr darr_ext(core::zero_t) noexcept {}
 
 			darr_ext(size_type count, const T& value) {
 				resize(count);
@@ -719,7 +704,7 @@ namespace gaia {
 				return mem::data_view_policy<T::gaia_Data_Layout, T>::template get<Item>(
 						std::span<const uint8_t>{(const uint8_t*)m_pData, capacity()});
 			}
-		};
+		}; // namespace cnt
 
 		namespace detail {
 			template <typename T, uint32_t N, uint32_t... I>

include/gaia/cnt/impl/sarray_ext_impl.h
@@ -142,22 +142,8 @@ namespace gaia {
 			size_type m_cnt = size_type(0);
 
 		public:
-			constexpr sarr_ext() noexcept {
-				if constexpr (!mem::is_soa_layout_v<T>)
-					core::call_ctor_raw_n(data(), extent);
-			}
-
-			//! Zero-initialization constructor. Because sarr_ext is not aggretate type, doing: sarr_ext<int,10> tmp{} does
-			//! not zero-initialize its internals. We need to be explicit about our intent and use a special constructor.
-			constexpr sarr_ext(core::zero_t) noexcept {
-				// explicit zeroing
-				if constexpr (!mem::is_soa_layout_v<T>)
-					core::call_ctor_n(data(), extent);
-				else {
-					for (auto i = (size_type)0; i < N; ++i)
-						operator[](i) = {};
-				}
-			}
+			constexpr sarr_ext() noexcept = default;
+			constexpr sarr_ext(core::zero_t) noexcept {}
 
 			~sarr_ext() {
 				if constexpr (!mem::is_soa_layout_v<T>)

include/gaia/cnt/impl/sarray_impl.h
@@ -142,7 +142,6 @@ namespace gaia {
 				if constexpr (!mem::is_soa_layout_v<T>)
 					core::call_ctor_raw_n(data(), extent);
 			}
-			
 
 			//! Zero-initialization constructor. Because sarr is not aggretate type, doing: sarr<int,10> tmp{} does not
 			//! zero-initialize its internals. We need to be explicit about our intent and use a special constructor.
@@ -151,7 +150,7 @@ namespace gaia {
 				if constexpr (!mem::is_soa_layout_v<T>)
 					core::call_ctor_n(data(), extent);
 				else {
-					for (auto i = (size_type)0; i < N; ++i)
+					for (auto i = (size_type)0; i < extent; ++i)
 						operator[](i) = {};
 				}
 			}

include/gaia/ecs/archetype.h
@@ -5,9 +5,7 @@
 #include <cstdint>
 
 #include "../cnt/darray.h"
-#include "../cnt/dbitset.h"
-#include "../cnt/sarray.h"
-#include "../cnt/sarray_ext.h"
+// #include "../cnt/dbitset.h"
 #include "../core/hashing_policy.h"
 #include "../mem/mem_alloc.h"
 #include "api.h"
@@ -104,8 +102,6 @@ namespace gaia {
 			};
 
 		private:
-			using AsPairsIndexBuffer = cnt::sarray<uint8_t, ChunkHeader::MAX_COMPONENTS>;
-
 			ArchetypeIdLookupKey::LookupHash m_archetypeIdHash;
 			//! Hash of components within this archetype - used for lookups
 			LookupHash m_hashLookup = {0};

include/gaia/ecs/world.h
@@ -8,7 +8,6 @@
 #include "../cnt/darray.h"
 #include "../cnt/darray_ext.h"
 #include "../cnt/map.h"
-#include "../cnt/sarray.h"
 #include "../cnt/sarray_ext.h"
 #include "../cnt/set.h"
 #include "../config/profiler.h"
@@ -18,7 +17,6 @@
 #include "../core/string.h"
 #include "../core/utility.h"
 #include "../mem/mem_alloc.h"
-#include "../meta/type_info.h"
 #include "api.h"
 #include "archetype.h"
 #include "archetype_common.h"

single_include/gaia.h
@@ -6812,23 +6812,8 @@ namespace gaia {
 			}
 
 		public:
-			darr_ext() noexcept {
-				if constexpr (!mem::is_soa_layout_v<T>)
-					core::call_ctor_raw_n(data(), extent);
-			}
-
-			//! Zero-initialization constructor. Because darr_ext is not aggretate type, doing: darr_ext<int,10> tmp{} does
-			//! not zero-initialize its internals. We need to be explicit about our intent and use a special constructor.
-			constexpr darr_ext(core::zero_t) noexcept
-			{
-				// explicit zeroing
-				if constexpr (!mem::is_soa_layout_v<T>)
-					core::call_ctor_n(data(), extent);
-				else {
-					for (auto i = (size_type)0; i < N; ++i)
-						operator[](i) = {};
-				}
-			}
+			darr_ext() noexcept = default;
+			constexpr darr_ext(core::zero_t) noexcept {}
 
 			darr_ext(size_type count, const T& value) {
 				resize(count);
@@ -7350,7 +7335,7 @@ namespace gaia {
 				return mem::data_view_policy<T::gaia_Data_Layout, T>::template get<Item>(
 						std::span<const uint8_t>{(const uint8_t*)m_pData, capacity()});
 			}
-		};
+		}; // namespace cnt
 
 		namespace detail {
 			template <typename T, uint32_t N, uint32_t... I>
@@ -11119,7 +11104,7 @@ namespace gaia {
 				if constexpr (!mem::is_soa_layout_v<T>)
 					core::call_ctor_n(data(), extent);
 				else {
-					for (auto i = (size_type)0; i < N; ++i)
+					for (auto i = (size_type)0; i < extent; ++i)
 						operator[](i) = {};
 				}
 			}
@@ -12821,22 +12806,8 @@ namespace gaia {
 			size_type m_cnt = size_type(0);
 
 		public:
-			constexpr sarr_ext() noexcept {
-				if constexpr (!mem::is_soa_layout_v<T>)
-					core::call_ctor_raw_n(data(), extent);
-			}
-
-			//! Zero-initialization constructor. Because sarr_ext is not aggretate type, doing: sarr_ext<int,10> tmp{} does
-			//! not zero-initialize its internals. We need to be explicit about our intent and use a special constructor.
-			constexpr sarr_ext(core::zero_t) noexcept {
-				// explicit zeroing
-				if constexpr (!mem::is_soa_layout_v<T>)
-					core::call_ctor_n(data(), extent);
-				else {
-					for (auto i = (size_type)0; i < N; ++i)
-						operator[](i) = {};
-				}
-			}
+			constexpr sarr_ext() noexcept = default;
+			constexpr sarr_ext(core::zero_t) noexcept {}
 
 			~sarr_ext() {
 				if constexpr (!mem::is_soa_layout_v<T>)
@@ -18182,6 +18153,8 @@ namespace gaia {
 #include <cinttypes>
 #include <cstdint>
 
+// #include "../cnt/dbitset.h"
+
 
 /*** Start of inlined file: api.h ***/
 #pragma once
@@ -22923,8 +22896,6 @@ namespace gaia {
 			};
 
 		private:
-			using AsPairsIndexBuffer = cnt::sarray<uint8_t, ChunkHeader::MAX_COMPONENTS>;
-
 			ArchetypeIdLookupKey::LookupHash m_archetypeIdHash;
 			//! Hash of components within this archetype - used for lookups
 			LookupHash m_hashLookup = {0};