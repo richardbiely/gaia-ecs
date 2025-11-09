richardbiely/gaia-ecs

commit 464aed93ee992a7800f07a6cb85c629332c89210

Changed: Serialization split into compile-time and run-time

include/gaia/ecs/query.h
@@ -13,7 +13,8 @@
 #include "../core/string.h"
 #include "../core/utility.h"
 #include "../mt/threadpool.h"
-#include "../ser/serialization.h"
+#include "../ser/ser_buffer_binary.h"
+#include "../ser/ser_ct.h"
 #include "api.h"
 #include "archetype.h"
 #include "archetype_common.h"
@@ -22,7 +23,6 @@
 #include "common.h"
 #include "component.h"
 #include "component_cache.h"
-#include "data_buffer.h"
 #include "id.h"
 #include "query_cache.h"
 #include "query_common.h"
@@ -367,7 +367,7 @@ namespace gaia {
 					uint16_t to;
 				};
 
-				using CmdBuffer = SerializationBufferDyn;
+				using CmdBuffer = ser::ser_buffer_binary;
 				using ChunkSpan = std::span<const Chunk*>;
 				using ChunkSpanMut = std::span<Chunk*>;
 				using ChunkBatchArray = cnt::sarray_ext<ChunkBatch, ChunkBatchSize>;

include/gaia/ecs/query_fwd.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <cstdint>
 
-#include "data_buffer_fwd.h"
-
 namespace gaia {
+	namespace ser {
+		class ser_buffer_binary;
+	}
+
 	namespace ecs {
 		class World;
 		class Archetype;
@@ -11,7 +13,7 @@ namespace gaia {
 
 		using QueryId = uint32_t;
 		using GroupId = uint32_t;
-		using QuerySerBuffer = SerializationBufferDyn;
+		using QuerySerBuffer = ser::ser_buffer_binary;
 
 		using TSortByFunc = int (*)(const World&, const void*, const void*);
 		using TGroupByFunc = GroupId (*)(const World&, const Archetype&, Entity);

include/gaia/ser/ser_buffer_binary.h
@@ -4,16 +4,17 @@
 #include <type_traits>
 
 #include "../cnt/darray_ext.h"
-#include "../mem/mem_utils.h"
-#include "component_cache_item.h"
 
 namespace gaia {
-	namespace ecs {
+	namespace ser {
+		enum class serialization_type_id : uint8_t;
+
 		namespace detail {
 			static constexpr uint32_t SerializationBufferCapacityIncreaseSize = 128U;
 
 			template <typename DataContainer>
-			class SerializationBufferImpl {
+			class ser_buffer_binary_impl {
+			protected:
 				// Increase the capacity by multiples of CapacityIncreaseSize
 				static constexpr uint32_t CapacityIncreaseSize = SerializationBufferCapacityIncreaseSize;
 
@@ -64,6 +65,11 @@ namespace gaia {
 					m_dataPos = pos;
 				}
 
+				//! Advances \param size bytes from the current buffer position
+				void skip(uint32_t size) {
+					m_dataPos += size;
+				}
+
 				//! Returns the current position in the buffer
 				GAIA_NODISCARD uint32_t tell() const {
 					return m_dataPos;
@@ -82,7 +88,10 @@ namespace gaia {
 				}
 
 				//! Writes \param size bytes of data starting at the address \param pSrc to the buffer
-				void save(const void* pSrc, uint32_t size) {
+				void save_raw(const void* pSrc, uint32_t size, [[maybe_unused]] ser::serialization_type_id id) {
+					if (size == 0)
+						return;
+
 					reserve(size);
 
 					// Copy "size" bytes of raw data starting at pSrc
@@ -90,29 +99,6 @@ namespace gaia {
 					memcpy((void*)&m_data[m_dataPos], pSrc, size);
 
 					m_dataPos += size;
-				}
-
-				//! Writes \param value to the buffer
-				template <typename T>
-				void save_comp(const ComponentCacheItem& item, T&& value) {
-					const bool isManualDestroyNeeded = item.func_copy_ctor != nullptr || item.func_move_ctor != nullptr;
-					constexpr bool isRValue = std::is_rvalue_reference_v<decltype(value)>;
-
-					reserve(sizeof(isManualDestroyNeeded) + sizeof(T));
-					save(isManualDestroyNeeded);
-					m_data.resize(m_dataPos + sizeof(T));
-
-					auto* pSrc = (void*)&value; // TODO: GAIA_FWD(value)?
-					auto* pDst = (void*)&m_data[m_dataPos];
-					if (isRValue && item.func_move_ctor != nullptr) {
-						if constexpr (mem::is_movable<T>())
-							mem::detail::move_ctor_element_aos<T>((T*)pDst, (T*)pSrc, 0, 0);
-						else
-							mem::detail::copy_ctor_element_aos<T>((T*)pDst, (const T*)pSrc, 0, 0);
-					} else
-						mem::detail::copy_ctor_element_aos<T>((T*)pDst, (const T*)pSrc, 0, 0);
-
-					m_dataPos += sizeof(T);
 				}
 
 				//! Loads \param value from the buffer
@@ -127,7 +113,10 @@ namespace gaia {
 				}
 
 				//! Loads \param size bytes of data from the buffer and writes them to the address \param pDst
-				void load(void* pDst, uint32_t size) {
+				void load_raw(void* pDst, uint32_t size, [[maybe_unused]] ser::serialization_type_id id) {
+					if (size == 0)
+						return;
+
 					GAIA_ASSERT(m_dataPos + size <= bytes());
 
 					const auto& cdata = std::as_const(m_data);
@@ -135,28 +124,13 @@ namespace gaia {
 
 					m_dataPos += size;
 				}
-
-				//! Loads \param value from the buffer
-				void load_comp(const ComponentCacheItem& item, void* pDst) {
-					bool isManualDestroyNeeded = false;
-					load(isManualDestroyNeeded);
-
-					GAIA_ASSERT(m_dataPos + item.comp.size() <= bytes());
-					const auto& cdata = std::as_const(m_data);
-					auto* pSrc = (void*)&cdata[m_dataPos];
-					item.move(pDst, pSrc, 0, 0, 1, 1);
-					if (isManualDestroyNeeded)
-						item.dtor(pSrc);
-
-					m_dataPos += item.comp.size();
-				}
 			};
 		} // namespace detail
 
-		using SerializationBuffer_DArrExt = cnt::darray_ext<uint8_t, detail::SerializationBufferCapacityIncreaseSize>;
-		using SerializationBuffer_DArr = cnt::darray<uint8_t>;
+		using ser_buffer_binary_storage = gaia::cnt::darray_ext<uint8_t, detail::SerializationBufferCapacityIncreaseSize>;
 
-		class SerializationBuffer: public detail::SerializationBufferImpl<SerializationBuffer_DArrExt> {};
-		class SerializationBufferDyn: public detail::SerializationBufferImpl<SerializationBuffer_DArr> {};
-	} // namespace ecs
+		//! Minimal binary serializer meant to runtime data.
+		//! It does not offer any versioning, or type information.
+		class ser_buffer_binary: public detail::ser_buffer_binary_impl<ser_buffer_binary_storage> {};
+	} // namespace ser
 } // namespace gaia

include/gaia.h
@@ -39,7 +39,8 @@
 #include "gaia/util/logging.h"
 #include "gaia/util/signal.h"
 
-#include "gaia/ser/serialization.h"
+#include "gaia/ser/ser_ct.h"
+#include "gaia/ser/ser_rt.h"
 
 #include "gaia/mt/threadpool.h"
 

include/gaia/config/config.h
@@ -103,11 +103,6 @@
 	#define GAIA_USE_PARTITIONED_BLOOM_FILTER 1
 #endif
 
-//! If enabled, ECS world serialization is enabled
-#ifndef GAIA_USE_SERIALIZATION
-	#define GAIA_USE_SERIALIZATION 1
-#endif
-
 //------------------------------------------------------------------------------
 
 #include "config_core_end.h"

include/gaia/ecs/api.inl
@@ -1,7 +1,5 @@
 #include "../config/config.h"
 
-#include "../ser/serialization.h"
-
 namespace gaia {
 	namespace ecs {
 		// Component API
@@ -134,16 +132,5 @@ namespace gaia {
 				return;
 			cmd_buffer_commit(world.cmd_buffer_mt());
 		}
-
-		// Serialization API
-
-#if GAIA_ASSERT_ENABLED
-		template <typename T>
-		void ser_check(const T& data) {
-			ecs::SerializationBuffer tmpBuffer;
-			ser::check(tmpBuffer, data);
-		}
-#endif
-
 	} // namespace ecs
 } // namespace gaia

include/gaia/ecs/archetype.h
@@ -16,9 +16,9 @@
 #include "chunk_header.h"
 #include "component.h"
 #include "component_cache.h"
-#include "data_buffer.h"
 #include "id.h"
 #include "query_mask.h"
+#include "ser_binary.h"
 
 namespace gaia {
 	namespace ecs {
@@ -277,9 +277,7 @@ namespace gaia {
 			Archetype& operator=(Archetype&&) = delete;
 			Archetype& operator=(const Archetype&) = delete;
 
-#if GAIA_USE_SERIALIZATION
-
-			void save(SerializationBufferDyn& s) {
+			void save(ser::ISerializer& s) {
 				s.save(m_firstFreeChunkIdx);
 				s.save(m_listIdx);
 
@@ -300,14 +298,14 @@ namespace gaia {
 				}
 			}
 
-			void load(SerializationBufferDyn& s) {
+			void load(ser::ISerializer& s) {
 				s.load(m_firstFreeChunkIdx);
 				s.load(m_listIdx);
 
 				uint32_t chunkCnt = 0;
 				s.load(chunkCnt);
 				{
-					const auto chunkCnt0 = m_chunks.size();
+					const auto chunkCnt0 = (uint32_t)m_chunks.size();
 					m_chunks.resize(chunkCnt);
 					// Make sure new chunks are set to nullptr
 					GAIA_FOR2(chunkCnt0, chunkCnt) m_chunks[i] = nullptr;
@@ -346,8 +344,6 @@ namespace gaia {
 					GAIA_ASSERT(chunkIdx == core::get_index(m_chunks, pChunk));
 				}
 			}
-
-#endif
 
 			void list_idx(uint32_t idx) {
 				m_listIdx = idx;

commit dd63fed4f71032258d4d7ab70bb6487103710b0e

Fixed: Memory access issue regression from 464aed9

include/gaia/ecs/query.h
@@ -367,40 +367,39 @@ namespace gaia {
 					uint16_t to;
 				};
 
-				using CmdBuffer = ser::ser_buffer_binary;
 				using ChunkSpan = std::span<const Chunk*>;
 				using ChunkSpanMut = std::span<Chunk*>;
 				using ChunkBatchArray = cnt::sarray_ext<ChunkBatch, ChunkBatchSize>;
-				using CmdFunc = void (*)(CmdBuffer& buffer, QueryCtx& ctx);
+				using CmdFunc = void (*)(QuerySerBuffer& buffer, QueryCtx& ctx);
 
 			private:
 				static constexpr CmdFunc CommandBufferRead[] = {
 						// Add item
-						[](CmdBuffer& buffer, QueryCtx& ctx) {
+						[](QuerySerBuffer& buffer, QueryCtx& ctx) {
 							QueryCmd_AddItem cmd;
 							ser::load(buffer, cmd);
 							cmd.exec(ctx);
 						},
 						// Add filter
-						[](CmdBuffer& buffer, QueryCtx& ctx) {
+						[](QuerySerBuffer& buffer, QueryCtx& ctx) {
 							QueryCmd_AddFilter cmd;
 							ser::load(buffer, cmd);
 							cmd.exec(ctx);
 						},
 						// SortBy
-						[](CmdBuffer& buffer, QueryCtx& ctx) {
+						[](QuerySerBuffer& buffer, QueryCtx& ctx) {
 							QueryCmd_SortBy cmd;
 							ser::load(buffer, cmd);
 							cmd.exec(ctx);
 						},
 						// GroupBy
-						[](CmdBuffer& buffer, QueryCtx& ctx) {
+						[](QuerySerBuffer& buffer, QueryCtx& ctx) {
 							QueryCmd_GroupBy cmd;
 							ser::load(buffer, cmd);
 							cmd.exec(ctx);
 						},
 						// SetGroupId
-						[](CmdBuffer& buffer, QueryCtx& ctx) {
+						[](QuerySerBuffer& buffer, QueryCtx& ctx) {
 							QueryCmd_SetGroupId cmd;
 							ser::load(buffer, cmd);
 							cmd.exec(ctx);

include/gaia/ecs/query_fwd.h
@@ -3,7 +3,7 @@
 
 namespace gaia {
 	namespace ser {
-		class ser_buffer_binary;
+		class ser_buffer_binary_dyn;
 	}
 
 	namespace ecs {
@@ -13,7 +13,7 @@ namespace gaia {
 
 		using QueryId = uint32_t;
 		using GroupId = uint32_t;
-		using QuerySerBuffer = ser::ser_buffer_binary;
+		using QuerySerBuffer = ser::ser_buffer_binary_dyn;
 
 		using TSortByFunc = int (*)(const World&, const void*, const void*);
 		using TGroupByFunc = GroupId (*)(const World&, const Archetype&, Entity);

include/gaia/ser/ser_buffer_binary.h
@@ -3,6 +3,7 @@
 
 #include <type_traits>
 
+#include "gaia/cnt/darray.h"
 #include "gaia/cnt/darray_ext.h"
 
 namespace gaia {
@@ -128,9 +129,11 @@ namespace gaia {
 		} // namespace detail
 
 		using ser_buffer_binary_storage = gaia::cnt::darray_ext<uint8_t, detail::SerializationBufferCapacityIncreaseSize>;
+		using ser_buffer_binary_storage_dyn = gaia::cnt::darray<uint8_t>;
 
 		//! Minimal binary serializer meant to runtime data.
 		//! It does not offer any versioning, or type information.
 		class ser_buffer_binary: public detail::ser_buffer_binary_impl<ser_buffer_binary_storage> {};
+		class ser_buffer_binary_dyn: public detail::ser_buffer_binary_impl<ser_buffer_binary_storage_dyn> {};
 	} // namespace ser
 } // namespace gaia

single_include/gaia.h
@@ -20571,7 +20571,7 @@ namespace gaia {
 
 namespace gaia {
 	namespace ser {
-		class ser_buffer_binary;
+		class ser_buffer_binary_dyn;
 	}
 
 	namespace ecs {
@@ -20581,7 +20581,7 @@ namespace gaia {
 
 		using QueryId = uint32_t;
 		using GroupId = uint32_t;
-		using QuerySerBuffer = ser::ser_buffer_binary;
+		using QuerySerBuffer = ser::ser_buffer_binary_dyn;
 
 		using TSortByFunc = int (*)(const World&, const void*, const void*);
 		using TGroupByFunc = GroupId (*)(const World&, const Archetype&, Entity);
@@ -23557,10 +23557,12 @@ namespace gaia {
 		} // namespace detail
 
 		using ser_buffer_binary_storage = gaia::cnt::darray_ext<uint8_t, detail::SerializationBufferCapacityIncreaseSize>;
+		using ser_buffer_binary_storage_dyn = gaia::cnt::darray<uint8_t>;
 
 		//! Minimal binary serializer meant to runtime data.
 		//! It does not offer any versioning, or type information.
 		class ser_buffer_binary: public detail::ser_buffer_binary_impl<ser_buffer_binary_storage> {};
+		class ser_buffer_binary_dyn: public detail::ser_buffer_binary_impl<ser_buffer_binary_storage_dyn> {};
 	} // namespace ser
 } // namespace gaia
 
@@ -29987,40 +29989,39 @@ namespace gaia {
 					uint16_t to;
 				};
 
-				using CmdBuffer = ser::ser_buffer_binary;
 				using ChunkSpan = std::span<const Chunk*>;
 				using ChunkSpanMut = std::span<Chunk*>;
 				using ChunkBatchArray = cnt::sarray_ext<ChunkBatch, ChunkBatchSize>;
-				using CmdFunc = void (*)(CmdBuffer& buffer, QueryCtx& ctx);
+				using CmdFunc = void (*)(QuerySerBuffer& buffer, QueryCtx& ctx);
 
 			private:
 				static constexpr CmdFunc CommandBufferRead[] = {
 						// Add item
-						[](CmdBuffer& buffer, QueryCtx& ctx) {
+						[](QuerySerBuffer& buffer, QueryCtx& ctx) {
 							QueryCmd_AddItem cmd;
 							ser::load(buffer, cmd);
 							cmd.exec(ctx);
 						},
 						// Add filter
-						[](CmdBuffer& buffer, QueryCtx& ctx) {
+						[](QuerySerBuffer& buffer, QueryCtx& ctx) {
 							QueryCmd_AddFilter cmd;
 							ser::load(buffer, cmd);
 							cmd.exec(ctx);
 						},
 						// SortBy
-						[](CmdBuffer& buffer, QueryCtx& ctx) {
+						[](QuerySerBuffer& buffer, QueryCtx& ctx) {
 							QueryCmd_SortBy cmd;
 							ser::load(buffer, cmd);
 							cmd.exec(ctx);
 						},
 						// GroupBy
-						[](CmdBuffer& buffer, QueryCtx& ctx) {
+						[](QuerySerBuffer& buffer, QueryCtx& ctx) {
 							QueryCmd_GroupBy cmd;
 							ser::load(buffer, cmd);
 							cmd.exec(ctx);
 						},
 						// SetGroupId
-						[](CmdBuffer& buffer, QueryCtx& ctx) {
+						[](QuerySerBuffer& buffer, QueryCtx& ctx) {
 							QueryCmd_SetGroupId cmd;
 							ser::load(buffer, cmd);
 							cmd.exec(ctx);