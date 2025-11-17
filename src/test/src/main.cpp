5d48cbb463300d2e3b4f8605c5c3cc73707bf505
Richard Biely <richardbiely@gmail.com>
2025-11-18 00:43:49 +0400

    Fixed: Forgotten debug comments

--- src/test/src/main.cpp
+++ src/test/src/main.cpp
@@ -10708,8 +10708,8 @@ TEST_CASE("Multithreading - Systems") {
 
 int main(int argc, char** argv) {
 	// Use custom logging. Just for code coverage.
-	// util::set_log_func(util::detail::log_cached);
-	// util::set_log_flush_func(util::detail::log_flush_cached);
+	util::set_log_func(util::detail::log_cached);
+	util::set_log_flush_func(util::detail::log_flush_cached);
 
 	doctest::Context ctx(argc, argv);
 	ctx.setOption("success", false); // suppress successful checks
