2304c3ed66fd208b21ebc6605ccb3521bfd5f350
Richard Biely <richardbiely@gmail.com>
2026-03-09 00:22:13 +0100

    Added: More query benchmarks

--- src/perf/matrix/src/main.cpp
+++ src/perf/matrix/src/main.cpp
@@ -2505,6 +2505,87 @@ void BM_Fragmented_Write(picobench::state& state) {
 	}
 }
 
+template <bool UseCachedQuery>
+void BM_Fragmented_QueryEach_Read1(picobench::state& state) {
+	ecs::World w;
+	// One entity per archetype isolates query iteration overhead on many tiny chunks.
+	create_fragmented_entities(w, 128U, 1U);
+
+	auto q = w.query<UseCachedQuery>().template all<Position>();
+	dont_optimize(q.empty());
+
+	for (auto _: state) {
+		(void)_;
+		uint64_t sum = 0ULL;
+		q.each([&](const Position& p) {
+			sum += (uint64_t)(p.x + p.y + p.z);
+		});
+		dont_optimize(sum);
+	}
+}
+
+template <bool UseCachedQuery>
+void BM_Fragmented_QueryEach_Read2(picobench::state& state) {
+	ecs::World w;
+	// One entity per archetype isolates query iteration overhead on many tiny chunks.
+	create_fragmented_entities(w, 128U, 1U);
+
+	auto q = w.query<UseCachedQuery>().template all<Position>().template all<Velocity>();
+	dont_optimize(q.empty());
+
+	for (auto _: state) {
+		(void)_;
+		uint64_t sum = 0ULL;
+		q.each([&](const Position& p, const Velocity& v) {
+			sum += (uint64_t)(p.x + p.y + p.z + v.x + v.y + v.z);
+		});
+		dont_optimize(sum);
+	}
+}
+
+template <bool UseCachedQuery>
+void BM_Fragmented_QueryEach_Write2(picobench::state& state) {
+	ecs::World w;
+	// Many tiny chunks make batching/setup cost more visible than raw component math.
+	create_fragmented_entities(w, 128U, 1U);
+
+	auto q = w.query<UseCachedQuery>().template all<Position&>().template all<Velocity>();
+	dont_optimize(q.empty());
+
+	for (auto _: state) {
+		(void)_;
+		q.each([](Position& p, const Velocity& v) {
+			p.x += v.x * DeltaTime;
+			p.y += v.y * DeltaTime;
+			p.z += v.z * DeltaTime;
+		});
+	}
+}
+
+void BM_Fragmented_QueryEach_Read1_Cached(picobench::state& state) {
+	BM_Fragmented_QueryEach_Read1<true>(state);
+}
+
+void BM_Fragmented_QueryEach_Read1_Uncached(picobench::state& state) {
+	BM_Fragmented_QueryEach_Read1<false>(state);
+}
+
+void BM_Fragmented_QueryEach_Read2_Cached(picobench::state& state) {
+	BM_Fragmented_QueryEach_Read2<true>(state);
+}
+
+void BM_Fragmented_QueryEach_Read2_Uncached(picobench::state& state) {
+	BM_Fragmented_QueryEach_Read2<false>(state);
+}
+
+void BM_Fragmented_QueryEach_Write2_Cached(picobench::state& state) {
+	BM_Fragmented_QueryEach_Write2<true>(state);
+}
+
+void BM_Fragmented_QueryEach_Write2_Uncached(picobench::state& state) {
+	BM_Fragmented_QueryEach_Write2<false>(state);
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Query creation / matching costs
 ////////////////////////////////////////////////////////////////////////////////
@@ -2965,6 +3046,12 @@ int main(int argc, char* argv[]) {
 		PICOBENCH_SUITE_REG("Fragmented archetypes");
 		PICOBENCH_REG(BM_Fragmented_Read).PICO_SETTINGS().label("read");
 		PICOBENCH_REG(BM_Fragmented_Write).PICO_SETTINGS().label("write");
+		PICOBENCH_REG(BM_Fragmented_QueryEach_Read1_Cached).PICO_SETTINGS_FOCUS().label("each cached 1c tiny");
+		PICOBENCH_REG(BM_Fragmented_QueryEach_Read1_Uncached).PICO_SETTINGS_FOCUS().label("each uncached 1c tiny");
+		PICOBENCH_REG(BM_Fragmented_QueryEach_Read2_Cached).PICO_SETTINGS_FOCUS().label("each cached 2c tiny");
+		PICOBENCH_REG(BM_Fragmented_QueryEach_Read2_Uncached).PICO_SETTINGS_FOCUS().label("each uncached 2c tiny");
+		PICOBENCH_REG(BM_Fragmented_QueryEach_Write2_Cached).PICO_SETTINGS_FOCUS().label("each cached rw tiny");
+		PICOBENCH_REG(BM_Fragmented_QueryEach_Write2_Uncached).PICO_SETTINGS_FOCUS().label("each uncached rw tiny");
 
 		PICOBENCH_SUITE_REG("Query build");
 		PICOBENCH_REG(BM_QueryBuild_Cached_2).PICO_SETTINGS_HEAVY().label("cached, 2 comp");
